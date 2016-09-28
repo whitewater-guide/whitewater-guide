f0107b2fabf1786121573d25317723b80e8cbbbe
doomsower <K.Kuznetcov@gmail.com>
2016-09-28 10:38:32 +0300

    Sign out

--- imports/ui/components/UserMenu.js
+++ imports/ui/components/UserMenu.js
@@ -4,46 +4,92 @@ import { ToolbarGroup } from 'material-ui/Toolbar';
 import { Meteor } from 'meteor/meteor';
 import Avatar from 'material-ui/Avatar';
 import FontIcon from 'material-ui/FontIcon';
+import Popover from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
 import { createContainer } from 'meteor/react-meteor-data';
 
 class UserMenu extends Component {
   static propTypes = {
     user: PropTypes.object,
+    loggingIn: PropTypes.bool,
   };
-  
+
+  state = {
+    menuOpen: false,
+  };
+
   render() {
     return (
       <ToolbarGroup>
         <IconButton onTouchTap={this.onAvatarPress}>
-          { this.getAvatar() }
+          {this.getAvatar()}
         </IconButton>
+        {this.props.user && this.renderPopover()}
       </ToolbarGroup>
     );
   }
 
+  renderPopover = () => {
+    return (
+      <Popover
+        open={this.state.menuOpen}
+        anchorEl={this.state.anchorEl}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+        onRequestClose={this.onPopoverClose} >
+        <Menu>
+          <MenuItem primaryText="Sign out" onTouchTap={this.signOut}/>
+        </Menu>
+      </Popover>
+    );
+  };
+
+  onPopoverClose = () => {
+    this.setState({
+      menuOpen: false,
+    });
+  };
+
   getAvatar = () => {
     const avatarProps = {};
-    if (this.props.user){
+    if (this.props.loggingIn){
+      return null;
+    }
+    else if (this.props.user) {
       avatarProps.src = `//graph.facebook.com/${this.props.user.services.facebook.id}/picture`;
     }
     else {
       avatarProps.icon = (
-        <FontIcon className="fa fa-sign-in"/>
+        <FontIcon className="fa fa-sign-in" />
       );
     }
     return (
-      <Avatar {...avatarProps}/>
+      <Avatar {...avatarProps} />
     );
   };
 
-  onAvatarPress = () => {
-    Meteor.loginWithFacebook(
-      {requestPermissions: ['email']}, 
-      (err) => {
-        if (err)
-          console.log('FB Error:', err);
-      }
-    );
+  onAvatarPress = (event) => {
+    event.preventDefault();
+    if (this.props.user){
+      this.setState({
+        menuOpen: true,
+        anchorEl: event.currentTarget,
+      });
+    }
+    else {
+      Meteor.loginWithFacebook(
+        { requestPermissions: ['email'] },
+        (err) => {
+          if (err)
+            console.log('FB Error:', err);
+        }
+      );
+    }
+  };
+
+  signOut = () => {
+    Meteor.logout((err) => err && console.log('Logout error:', err));
   };
 
 }
@@ -51,6 +97,7 @@ class UserMenu extends Component {
 export default createContainer(() => {
   const subscription = Meteor.subscribe('userData');
   return {
+    loggingIn: Meteor.loggingIn(),
     user: Meteor.user(),
   };
 }, UserMenu);
