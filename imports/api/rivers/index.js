5f4254cbbef48aa0d0ef0756f8bc262d37cbebb3
doomsower <K.Kuznetcov@gmail.com>
2016-10-26 17:01:45 +0300

    Rivers collection stub

---
+++ imports/api/rivers/index.js
@@ -0,0 +1,70 @@
+import { Mongo } from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import AdminMethod from '../../utils/AdminMethod';
+
+export const Rivers = new Mongo.Collection('rivers');
+
+const riversSchema = new SimpleSchema({
+  region: {
+    type: Meteor.ObjectID,
+    label: 'Region',
+    index: true,
+  },
+  name: {
+    type: String,
+    label: 'Name',
+  },
+  description: {
+    type: String,
+    label: 'Description',
+    optional: true,
+  },
+});
+
+Rivers.attachSchema(riversSchema);
+
+export const createRiver = new AdminMethod({
+  name: 'rivers.create',
+
+  validate: riversSchema.validator({ clean: true }),
+
+  applyOptions: {
+    noRetry: true,
+  },
+
+  run(data) {
+    return Rivers.insert(data);
+  }
+});
+
+export const editRiver = new AdminMethod({
+  name: 'rivers.edit',
+
+  validate: new SimpleSchema([riversSchema, { _id: { type: String, regEx: SimpleSchema.RegEx.Id } }]).validator({ clean: true }),
+
+  applyOptions: {
+    noRetry: true,
+  },
+
+  run({_id, ...data}) {
+    return Rivers.update(_id, { $set: {...data } } );
+  }
+});
+
+export const removeRiver = new AdminMethod({
+  name: 'rivers.remove',
+
+  validate: new SimpleSchema({
+    riverId: { type: Meteor.ObjectID }
+  }).validator(),
+
+  applyOptions: {
+    noRetry: true,
+  },
+  
+  run({riverId}) {
+    return Rivers.remove(riverId);
+  },
+  
+});
