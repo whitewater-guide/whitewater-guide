0d5e688a36da225aa89d7b04fe704cf2d52f52e9
doomsower <K.Kuznetcov@gmail.com>
2017-01-04 16:41:25 -0600

    Migration: set proper point kind for all gauge locations

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -2,3 +2,4 @@ import '../../tap-i18n';
 import './fixtures';
 import './accounts';
 import './register-api';
+import './migrations';
---
+++ imports/startup/server/migrations.js
@@ -0,0 +1,33 @@
+import {Meteor} from 'meteor/meteor';
+import {Migrations} from 'meteor/percolate:migrations';
+import {Gauges} from '../../api/gauges';
+import {Points} from '../../api/points';
+
+Migrations.add({
+  version: 1,
+  up: function migration1up() {
+    const gaugesBatch = Gauges.rawCollection().initializeUnorderedBulkOp();
+    const pointsBatch = Points.rawCollection().initializeUnorderedBulkOp();
+    let hasUpdates = false;
+    Gauges.find({}).forEach(gauge => {
+      if (gauge.location && gauge.location._id) {
+        hasUpdates = true;
+        gaugesBatch.find({_id: gauge._id}).updateOne({$set: {"location.kind": 'gauge'}});
+        pointsBatch.find({_id: gauge.location._id}).updateOne({$set: {kind: 'gauge'}});
+      }
+    });
+
+    if (hasUpdates) {
+      // We need to wrap the async function to get a synchronous API that migrations expects
+      const executeGaugesBatch = Meteor.wrapAsync(gaugesBatch.execute, gaugesBatch);
+      const executePointsBatch = Meteor.wrapAsync(pointsBatch.execute, pointsBatch);
+      return executeGaugesBatch() && executePointsBatch();
+    }
+
+    return true;
+  }
+});
+
+Meteor.startup(() => {
+  Migrations.migrateTo('latest');
+});
