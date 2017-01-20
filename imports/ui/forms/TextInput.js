5bd21f546e63cce8bf605a15f0e923f5b7155028
doomsower <K.Kuznetcov@gmail.com>
2017-01-20 20:16:01 +0300

    Make number text inputs locale-agnostic Closes #3

--- imports/ui/forms/TextInput.js
+++ imports/ui/forms/TextInput.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import TextField from 'material-ui/TextField';
+import {strToFloat} from '../../utils/TextUtils';
 
 class TextInput extends Component {
   static propTypes = {
@@ -13,19 +14,41 @@ class TextInput extends Component {
     type: PropTypes.string,
   };
 
+  constructor(props){
+    super(props);
+    this.state = {
+      value: String(props.field.value || ''),
+    };
+  }
+
+  componentWillReceiveProps(nextProps){
+    const nextVal = nextProps.field.value;
+    if (nextProps.type === 'number' && strToFloat(nextVal) !== strToFloat(this.state.value)){
+      const value = isNaN(nextVal) ? '' : String(nextVal);
+      this.setState({value});
+    }
+  }
+
   render() {
-    const value = this.props.field.value === undefined ? '' : this.props.field.value;
+    const {type, field, title} = this.props;
+    const numericProps = type === 'number' ? {pattern: '[0-9]+([\,|\.][0-9]+)?'} : {type};
     return (
       <TextField
         style={style}
-        type={this.props.type}
-        value={value}
-        errorText={this.props.field.error}
-        onChange={(e, value) => this.props.field.onChange(value)}
-        hintText={this.props.title}
-        floatingLabelText={this.props.title}
+        value={this.state.value}
+        errorText={field.error}
+        onChange={this.onChange}
+        hintText={title}
+        floatingLabelText={title}
+        {...numericProps}
       />
     );
+  }
+
+  onChange = (e, value) => {
+    this.setState({value});
+    const castValue = this.props.type === 'number' ? strToFloat(value) : value;
+    this.props.field.onChange(castValue);
   }
 }
 
--- imports/utils/TextUtils.js
+++ imports/utils/TextUtils.js
@@ -1,5 +1,10 @@
 const ROMAN_NUMBERS = ['0', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
 
+const DECIMAL_POINT = (1.1).toLocaleString().substr(1,1);
+let K_SEPARATOR = (5000).toLocaleString().substr(1,1);
+if (K_SEPARATOR === '0')
+  K_SEPARATOR = (DECIMAL_POINT === '.' ? ',' : '.');
+
 export function toRomanDifficulty(decimalDifficulty) {
   if (Number.isInteger(decimalDifficulty)){
     return ROMAN_NUMBERS[decimalDifficulty];
@@ -15,4 +20,81 @@ export function renderDifficulty({difficulty, difficultyXtra}) {
   if (difficultyXtra)
     result = result + ' (' + difficultyXtra + ')';
   return result;
+}
+
+/**
+ * locale semi-agnostic string to float conversion
+ * https://gist.github.com/GerHobbelt/2037124
+ * @param String with either "," or "." as decimal separator
+ * @returns Number
+ */
+export function strToFloat(str) {
+  switch (typeof(str)) {
+    case 'float':
+    case 'integer':
+      return str;
+
+    default:
+      str = '' + str; // force str to be string type
+      str = str.match(/[0-9.,eE-]+/);
+      if (str)
+        str = str[0];
+      else
+        return Number.NaN; // VERY illegal number format
+
+      var kp = str.indexOf(',');
+      var dp = str.indexOf('.');
+      var kpl = str.lastIndexOf(',');
+      var dpl = str.lastIndexOf('.');
+      // can we be 'locale agnostic'? We can if both markers are in the input:
+      if (kp > 0 && dp > 0) {
+        if (kp < dp) {
+          // e.g.: 1,000.00
+          if (kpl > dpl || dpl > dp)
+            return Number.NaN; // VERY illegal number format
+          str = str.replace(/,/g, '');
+        } else {
+          // e.g.: 1.000,00
+          if (kpl < dpl || kpl > kp)
+            return Number.NaN; // VERY illegal number format
+          str = str.replace(/\./g, '').replace(',', '.');
+        }
+      } else {
+        // only one of 'em in there: must we use the detected 'current' locale
+        // or can we 'heuristically determine' what's right here?
+        // We can do the latter if we have either:
+        // - only up to 2 digits following the only separator
+        // - more than 3 digits following the only separator
+        // - one separator only and the number starts with it, e.g. '.45'
+        //
+        // When we have only 3 digits following the last and only sep, we assume the separator
+        // to be a 'thousands marker'.
+        // We COULD fall back to the current locale, but than the ambiguous items receive
+        // different treatment on a possibly incorrect locale setting (e.g. my machines are all
+        // US/metric configured while I live in the Netherlands, but I /despise/ Dutch MS Excel,
+        // for example. Riding the 'locale aware' ticket would definitely screw up my [ambiguous]
+        // numbers. I believe it's better to be consistently wrong than semi-randomly correct.
+        //
+        // examples: 1.000 : 5,00 : 2,999.95 : 2,998 : 7.50 : 0,1791634 : 0.1791634 : .45 : ,32
+        if (dp >= 0 && kp < 0) {
+          // .45, ....
+          if (dp !== 0 && (dpl > dp || str.substr(dp + 1).match(/^[0-9]{3}\b/)))
+            str = str.replace(/\./g, '');
+        } else if (kp >= 0 && dp < 0) {
+          // ,32; ....
+          if (kp !== 0 && (kpl > kp || str.substr(kp + 1).match(/^[0-9]{3}\b/)))
+            str = str.replace(/,/g, '');
+          else
+            str = str.replace(',', '.');
+        } else if (kp < 0 && dp < 0) {
+          // integer value
+        } else {
+          // VERY illegal format, such as '.456,678'
+          return Number.NaN;
+        }
+      }
+      // now str has parseFloat() compliant format with US decimal point
+      // only (iff it has a decimal fraction at all).
+      return parseFloat(str);
+  }
 }
