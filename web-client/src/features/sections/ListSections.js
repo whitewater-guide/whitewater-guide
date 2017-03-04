1820afadf11aa1b36187809ca3e708146f09d75d
doomsower <K.Kuznetcov@gmail.com>
2017-03-04 20:43:25 +0300

    Extract sections table into own class

--- web-client/src/features/sections/ListSections.js
+++ web-client/src/features/sections/ListSections.js
@@ -1,11 +1,7 @@
 import React, {Component, PropTypes} from 'react';
-import {Column, Table, AutoSizer, SortDirection, InfiniteLoader} from 'react-virtualized';
-import {Rating} from '../../core/forms';
-import {Durations} from './Durations';
-import {AdminControls, rowRenderer} from '../../core/components';
-import {renderDifficulty} from '../../utils/TextUtils';
-import _ from 'lodash';
+import {AutoSizer, SortDirection, InfiniteLoader} from 'react-virtualized';
 import container from './ListSectionsContainer';
+import SectionsTable from './SectionsTable';
 
 class ListSections extends Component {
 
@@ -27,10 +23,8 @@ class ListSections extends Component {
     sortDirection: SortDirection.ASC,
   };
 
-  durationsMap = _.keyBy(Durations, 'value');
-
   render() {
-    const {admin, sections, count, loadMore} = this.props;
+    const {admin, sections, count, loadMore, sortBy, sortDirection, onSort} = this.props;
     return (
       <div style={styles.wrapper}>
         <AutoSizer>
@@ -41,30 +35,20 @@ class ListSections extends Component {
               rowCount={count}
             >
               {({onRowsRendered, registerChild}) => (
-                <Table
+                <SectionsTable
+                  admin={admin}
+                  sections={sections}
                   onRowsRendered={onRowsRendered}
-                  ref={registerChild}
+                  registerChild={registerChild}
                   width={width}
                   height={height}
-                  headerHeight={20}
-                  rowHeight={30}
-                  rowCount={sections.length}
-                  rowGetter={({index}) => sections[index]}
-                  sortBy={this.props.sortBy}
-                  sortDirection={this.props.sortDirection}
-                  sort={this.props.onSort}
-                  onRowClick={this.onRowClick}
-                  rowStyle={this.styleRow}
-                  rowRenderer={rowRenderer}
-                >
-                  <Column width={200} flexGrow={1} label='Name' dataKey="name" cellDataGetter={this.renderName}/>
-                  <Column width={110} label='Difficulty' dataKey="difficulty" cellDataGetter={this.difficultyRenderer}/>
-                  <Column width={130} label='Rating' dataKey="rating" cellRenderer={this.renderRating}/>
-                  <Column width={80} label='Drop (m)' dataKey="drop"/>
-                  <Column width={80} label='Length' dataKey="distance"/>
-                  <Column width={80} label='Duration' dataKey="duration" cellDataGetter={({rowData}) => _.get(this.durationsMap, [rowData.duration, 'slug'])}/>
-                  {admin && <Column width={90} dataKey="controls" label="Controls" cellRenderer={this.renderControls}/>}
-                </Table>
+                  sortBy={sortBy}
+                  sortDirection={sortDirection}
+                  sort={onSort}
+                  onEditSection={this.onEditSection}
+                  onDeleteSection={this.onDeleteSection}
+                  onSectionClick={this.onSectionClick}
+                />
               )}
             </InfiniteLoader>
           )}
@@ -73,40 +57,13 @@ class ListSections extends Component {
     );
   }
 
-  isRowLoaded = ({ index }) => {
-    return !!this.props.sections[index];
-  };
+  isRowLoaded = ({index}) => !!this.props.sections[index];
 
-  renderName = ({rowData}) => {
-    return `${rowData.river.name} - ${rowData.name}`;
-  };
+  onSectionClick = (sectionId) => this.props.push(`/sections/${sectionId}`);
 
-  difficultyRenderer = ({rowData}) => {
-    return renderDifficulty(rowData);
-  };
+  onEditSection = (sectionId) => this.props.push(`/sections/${sectionId}/settings`);
 
-  renderRating = ({rowData}) => {
-    const field = {value: rowData.rating};
-    return (
-      <Rating field={field} style={styles.rating}/>
-    );
-  };
-
-  renderControls = ({rowData}) => {
-    const editHandler = () => this.props.push(`/sections/${rowData._id}/settings`);
-    const deleteHandler = () => this.props.removeSection(rowData._id);
-    return (
-      <AdminControls onEdit={editHandler} onDelete={deleteHandler}/>
-    );
-  };
-
-  onRowClick = ({index}) => this.props.push(`/sections/${this.props.sections[index]._id}`);
-
-  styleRow = ({index}) => {
-    return {
-
-    }
-  };
+  onDeleteSection = (sectionId) => this.props.removeSection(sectionId);
 
 }
 
@@ -115,9 +72,6 @@ const styles = {
     width: '100%',
     height: '100%',
     overflow: 'hidden',
-  },
-  rating: {
-    minWidth: 10,
   },
 };
 
