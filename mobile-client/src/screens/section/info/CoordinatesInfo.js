245ed67525edbf166bab09fd325bff2b7d09931f
doomsower <K.Kuznetcov@gmail.com>
2017-04-06 15:44:06 +0300

    Extracted put-in and take-out rows into separate components and make buttons work

---
+++ mobile-client/src/screens/section/info/CoordinatesInfo.js
@@ -0,0 +1,34 @@
+import React, { PropTypes } from 'react';
+import { Clipboard, Linking, View } from 'react-native';
+import { Button, Icon, Left, ListItem, Text } from 'native-base';
+import { arrayToPrettyDMS } from '../../../commons/utils/GeoUtils';
+
+const CoordinatesInfo = ({ label, coordinates }) => {
+  const prettyCoord = arrayToPrettyDMS(coordinates);
+  const copyHandler = () => Clipboard.setString(prettyCoord);
+  const direcionsURL = `https://www.google.com/maps/dir/Current+Location/${coordinates[1]},${coordinates[0]}`;
+  const directionsHandler = () => Linking.openURL(direcionsURL).catch(() => {});
+  return (
+    <ListItem>
+      <Left><Text>{label}</Text></Left>
+      <View style={{ paddingRight: 12 }}>
+        <Text note>{prettyCoord}</Text>
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+        <Button transparent style={{ paddingLeft: 0, paddingRight: 4 }} onPress={copyHandler}>
+          <Icon name="copy" style={{ color: 'black', fontSize: 21 }} />
+        </Button>
+        <Button transparent style={{ paddingLeft: 4, paddingRight: 0 }} onPress={directionsHandler}>
+          <Icon name="navigate" style={{ color: 'black', fontSize: 21 }} />
+        </Button>
+      </View>
+    </ListItem>
+  );
+};
+
+CoordinatesInfo.propTypes = {
+  label: PropTypes.string.isRequired,
+  coordinates: PropTypes.arrayOf(PropTypes.number).isRequired,
+};
+
+export default CoordinatesInfo;
--- mobile-client/src/screens/section/info/SectionInfoScreen.js
+++ mobile-client/src/screens/section/info/SectionInfoScreen.js
@@ -1,12 +1,11 @@
 import React, { PropTypes } from 'react';
-import { View } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 import { connect } from 'react-redux';
-import { List, ListItem, Left, Body, Right, Text, Button, Icon } from 'native-base';
+import { List, ListItem, Left, Body, Right, Text } from 'native-base';
 import StarRating from 'react-native-star-rating';
 import { renderDifficulty } from '../../../commons/utils/TextUtils';
-import { arrayToPrettyDMS } from '../../../commons/utils/GeoUtils';
 import { Screen } from '../../../components';
+import CoordinatesInfo from './CoordinatesInfo';
 
 class SectionInfoScreen extends React.PureComponent {
 
@@ -22,8 +21,6 @@ class SectionInfoScreen extends React.PureComponent {
 
   render() {
     const { screenProps: { section = { river: {} }, sectionLoading } } = this.props;
-    const putIn = arrayToPrettyDMS(section.putIn.coordinates);
-    const takeOut = arrayToPrettyDMS(section.takeOut.coordinates);
     return (
       <Screen loading={sectionLoading}>
         <List>
@@ -60,35 +57,9 @@ class SectionInfoScreen extends React.PureComponent {
             <Body><Text note>{section.season}</Text></Body>
           </ListItem>
 
-          <ListItem>
-            <Left><Text>Put-in</Text></Left>
-            <View style={{ paddingRight: 12 }}>
-              <Text note>{putIn}</Text>
-            </View>
-            <View style={{ flexDirection: 'row' }}>
-              <Button transparent style={{ paddingLeft: 0, paddingRight: 4 }}>
-                <Icon name="copy" style={{ color: 'black', fontSize: 21 }} />
-              </Button>
-              <Button transparent style={{ paddingLeft: 4, paddingRight: 0 }}>
-                <Icon name="navigate" style={{ color: 'black', fontSize: 21 }} />
-              </Button>
-            </View>
-          </ListItem>
+          <CoordinatesInfo label="Put-in" coordinates={section.putIn.coordinates} />
 
-          <ListItem>
-            <Left><Text>Take out</Text></Left>
-            <View style={{ paddingRight: 12 }}>
-              <Text note>{takeOut}</Text>
-            </View>
-            <View style={{ flexDirection: 'row' }}>
-              <Button transparent style={{ paddingLeft: 0, paddingRight: 4 }}>
-                <Icon name="copy" style={{ color: 'black', fontSize: 21 }} />
-              </Button>
-              <Button transparent style={{ paddingLeft: 4, paddingRight: 0 }}>
-                <Icon name="navigate" style={{ color: 'black', fontSize: 21 }} />
-              </Button>
-            </View>
-          </ListItem>
+          <CoordinatesInfo label="Take-out" coordinates={section.takeOut.coordinates} />
 
         </List>
       </Screen>
