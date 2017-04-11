6af15c10103728d64ea703a1618c66679e257be8
doomsower <K.Kuznetcov@gmail.com>
2017-04-11 18:10:06 +0300

    Some simple icons for POI

--- mobile-client/src/components/map/renderSimplePOI.js
+++ mobile-client/src/components/map/renderSimplePOI.js
@@ -1,5 +1,50 @@
 import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons'
 import MapView from 'react-native-maps';
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black',
+  },
+});
+
+const IconKinds = {
+  'put-in': 'arrow-downward',
+  'take-out': 'arrow-upward',
+  'put-in-alt': 'arrow-downward',
+  'put-in-road': 'local-parking',
+  'take-out-alt': 'arrow-upward',
+  'take-out-road': 'local-parking',
+  waterfall: 'adjust', // TODO: google maps online have icon for waterfall
+  rapid: 'adjust',
+  portage: 'directions-walk',
+  playspot: 'stars',
+  hazard: 'warning',
+  'river-campsite': 'adjust',
+  'wild-camping': 'adjust',
+  'paid-camping': 'adjust',
+  gauge: 'straighten',
+  'hike-waypoint': 'adjust',
+  bridge: 'adjust',
+  other: 'adjust',
+};
+
+const Anchor = { x: 0.5, y: 0.5 };
+
+const renderCustomMarkerView = (kind) => {
+  const icon = IconKinds[kind] || 'adjust';
+  return (
+    <View style={styles.iconContainer}>
+      <Icon name={icon} color="white" />
+    </View>
+  );
+};
 
 const renderSimplePOI = (poi, isSelected, selectPOI) => {
   const {
@@ -12,12 +57,15 @@ const renderSimplePOI = (poi, isSelected, selectPOI) => {
   // console.tron.log(poi);
   return (
     <MapView.Marker
+      anchor={Anchor}
       title={name || kind}
       description={description}
       onPress={selectPOI}
       key={_id}
       coordinate={{ longitude, latitude }}
-    />
+    >
+      { renderCustomMarkerView(kind) }
+    </MapView.Marker>
   );
 };
 
