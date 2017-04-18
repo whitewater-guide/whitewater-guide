04d849690737f1b69c4ff2fa1e8f6fd8ff872205
doomsower <K.Kuznetcov@gmail.com>
2017-04-18 08:44:05 +0300

    Eject native-base theme

--- mobile-client/src/App.js
+++ mobile-client/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { ApolloProvider } from 'react-apollo';
+import { StyleProvider } from 'native-base';
 import RootView from './core/RootView';
 import configureStore from './core/config/configureStore';
 import { apolloClient } from './core/config/configureApollo';
+import getTheme from './theme/components';
+import platform from './theme/variables/platform';
 
 const store = configureStore();
 
 export default function App() {
   return (
     <ApolloProvider store={store} client={apolloClient}>
-      <RootView />
+      <StyleProvider style={getTheme(platform)}>
+        <RootView />
+      </StyleProvider>
     </ApolloProvider>
   );
 }
