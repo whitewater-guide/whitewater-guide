d584c5349464e38d9e98ba1b28482316ab8efe28
doomsower <K.Kuznetcov@gmail.com>
2017-04-19 12:35:43 +0300

    Update sections list when new section is inserted

--- commons/features/sections/sectionsListReducer.js
+++ commons/features/sections/sectionsListReducer.js
@@ -12,10 +12,23 @@ function removeSection(previousResult, _id) {
   };
 }
 
+function upsertSection(previousResult, newSection) {
+  const { sections: { sections, count } } = previousResult;
+  return {
+    ...previousResult,
+    sections: {
+      sections: [...sections, newSection],
+      count: count + 1,
+    },
+  };
+}
+
 export default function (previousResult, action, variables) {
   // console.dir({ SECTION_REDUCER: 'SECTION_REDUCER', previousResult, action, variables });
   if (isMutationResult(action, 'removeSection') && action.result.data.removeSection === true) {
     return removeSection(previousResult, action.variables._id);
+  } else if (isMutationResult(action, 'upsertSection')) {
+    return upsertSection(previousResult, action.result.data.upsertSection);
   }
   return previousResult;
 }
