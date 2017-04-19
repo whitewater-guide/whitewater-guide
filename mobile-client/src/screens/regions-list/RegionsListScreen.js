2e10a9ae7075bb149b2c111188f0150ab0b8eceb
doomsower <K.Kuznetcov@gmail.com>
2017-04-19 13:39:12 +0300

    Show number of rivers and sections in region list

--- mobile-client/src/screens/regions-list/RegionsListScreen.js
+++ mobile-client/src/screens/regions-list/RegionsListScreen.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
 import { List, ListItem, Text, Body, Right, Icon } from 'native-base';
+import { RefreshControl, View } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 import { connect } from 'react-redux';
 import { compose, hoistStatics } from 'recompose';
@@ -10,6 +11,8 @@ import { Screen, withErrorsView, spinnerWhileLoading } from '../../components';
 class RegionsListScreen extends PureComponent {
   static propTypes = {
     regions: PropTypes.array,
+    regionsListLoading: PropTypes.bool.isRequired,
+    refetchRegionsList: PropTypes.func.isRequired,
     dispatch: PropTypes.func,
   };
 
@@ -28,6 +31,10 @@ class RegionsListScreen extends PureComponent {
     <ListItem button onPress={() => this.onRegionSelected(region)}>
       <Body>
         <Text>{region.name}</Text>
+        <View style={{ flexDirection: 'row' }}>
+          <Text note style={{ minWidth: 60 }}>{`Rivers: ${region.riversCount}`}</Text>
+          <Text note>{`Sections: ${region.sectionsCount}`}</Text>
+        </View>
       </Body>
       <Right>
         <Icon name="arrow-forward" />
@@ -36,9 +43,15 @@ class RegionsListScreen extends PureComponent {
   );
 
   render() {
+    const { regionsListLoading, refetchRegionsList } = this.props;
+    const refreshControl = <RefreshControl refreshing={regionsListLoading} onRefresh={refetchRegionsList} />;
     return (
       <Screen noScroll>
-        <List dataArray={this.props.regions} renderRow={this.renderRow} />
+        <List
+          dataArray={this.props.regions}
+          refreshControl={refreshControl}
+          renderRow={this.renderRow}
+        />
       </Screen>
     );
   }
