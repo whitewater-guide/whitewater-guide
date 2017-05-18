5db9f7c7648d56541d83bf5318d7bea38060c215
doomsower <K.Kuznetcov@gmail.com>
2017-05-18 20:49:08 +0400

    Make filters icon filled when filters are applied

--- mobile-client/src/screens/region/components/FilterButton.js
+++ mobile-client/src/screens/region/components/FilterButton.js
@@ -3,8 +3,11 @@ import PropTypes from 'prop-types';
 import { StyleSheet, TouchableOpacity } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 import { connect } from 'react-redux';
+import { isEqual, omit } from 'lodash';
 import Icon from 'react-native-vector-icons/Ionicons';
 import variables from '../../../theme/variables/platform';
+import { currentSectionSearchTerms } from '../../../core/selectors';
+import { defaultSectionSearchTerms } from '../../../commons/domain';
 
 const styles = StyleSheet.create({
   button: {
@@ -17,6 +20,7 @@ const styles = StyleSheet.create({
 class FilterButton extends React.PureComponent {
   static propTypes = {
     navigate: PropTypes.func.isRequired,
+    hasFilters: PropTypes.bool.isRequired,
   };
 
   onPress = () => {
@@ -24,12 +28,21 @@ class FilterButton extends React.PureComponent {
   }
 
   render() {
+    const iconName = this.props.hasFilters ? 'ios-funnel' : 'ios-funnel-outline';
     return (
       <TouchableOpacity style={styles.button} onPress={this.onPress}>
-        <Icon name="ios-funnel-outline" size={20} color={variables.btnPrimaryBg} />
+        <Icon name={iconName} size={20} color={variables.btnPrimaryBg} />
       </TouchableOpacity>
     );
   }
 }
 
-export default connect(undefined, NavigationActions)(FilterButton);
+export default connect(
+  state => ({
+    hasFilters: !isEqual(
+      omit(defaultSectionSearchTerms, ['searchString']),
+      omit(currentSectionSearchTerms(state), ['regionId', 'searchString']),
+    ),
+  }),
+  NavigationActions,
+)(FilterButton);
