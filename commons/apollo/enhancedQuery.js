4d169339912beeadb9b5f37bf9ebf3a9ec49f8cc
doomsower <K.Kuznetcov@gmail.com>
2017-05-18 15:18:33 +0400

    Do not show error view on n etwork error with cached data

--- commons/apollo/enhancedQuery.js
+++ commons/apollo/enhancedQuery.js
@@ -5,13 +5,19 @@ import _ from 'lodash';
  * Helper function to wrap error and refetch handlers of graphql containers and pass them down
  * Does not overwrite errors from other graphql containers. Instead, merges them.
  * @param errorPropName Name of grpahql container that caused error, it will be the key in errors object
+ * @param dataPropName If network error is present, but data in this props is also present (e.g. cached data), then swallow error
  * @param mapProps Function to map props when no error is found
  */
-const wrapErrors = (errorPropName, mapProps) => (props) => {
-  const { data: { error, refetch }, ownProps } = props;
+const wrapErrors = (errorPropName, dataPropName, mapProps) => (props) => {
+  const { data, ownProps } = props;
+  const { error, refetch } = data;
   let errors = ownProps.errors || {};
   if (error) {
-    errors = { ...errors, [errorPropName]: { error, refetch } };
+    if (error.networkError && data.hasOwnProperty(dataPropName)) {
+      console.log(`$Error ${error.errorMessage}`);
+    } else {
+      errors = { ...errors, [errorPropName]: { error, refetch } };
+    }
   }
   return { ...mapProps(props), errors };
 };
@@ -28,7 +34,8 @@ const enhancedQuery = (query, config) => {
   const mapProps = config && config.props;
   if (queryDef && _.isFunction(mapProps)) {
     const errorPropName = queryDef.name.value;
-    const wrappedMapProps = wrapErrors(errorPropName, mapProps);
+    const dataPropName = queryDef.selectionSet.selections[0].name.value;
+    const wrappedMapProps = wrapErrors(errorPropName, dataPropName, mapProps);
     return graphql(query, { ...config, props: wrappedMapProps });
   }
   return graphql(query, config);
