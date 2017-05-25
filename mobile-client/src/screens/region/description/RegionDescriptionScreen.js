1ed76b1e18fbe5fe0354b5c4e40b5c9c05aa8307
doomsower <K.Kuznetcov@gmail.com>
2017-05-25 17:11:13 +0400

    Added no region dewscription message

--- mobile-client/src/screens/region/description/RegionDescriptionScreen.js
+++ mobile-client/src/screens/region/description/RegionDescriptionScreen.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { NavigationActions } from 'react-navigation';
 import { connect } from 'react-redux';
 import { HTMLView, Screen } from '../../../components';
+import NoRegionDescription from './NoRegionDescription';
 
 class RegionDescriptionScreen extends React.PureComponent {
 
@@ -16,9 +17,12 @@ class RegionDescriptionScreen extends React.PureComponent {
 
   render() {
     const { screenProps: { region = {} } } = this.props;
+    console.log('Region description', region.description ? 'a' : 'b');
     return (
-      <Screen>
-        <HTMLView value={region.description} />
+      <Screen noScroll={!region.description}>
+        {
+          region.description ? (<HTMLView value={region.description} />) : (<NoRegionDescription />)
+        }
       </Screen>
     );
   }
