3d0edd1f08f0fbbe3df6d13dedd3c17de2d01669
doomsower <K.Kuznetcov@gmail.com>
2017-06-15 22:28:59 +0300

    Do not use glamorous theme provider, see #92

--- mobile-client/src/App.js
+++ mobile-client/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import { PortalProvider } from 'react-native-portal';
-import { ThemeProvider } from 'glamorous-native';
 import RootView from './core/RootView';
 import configureStore from './core/config/configureStore';
 import { apolloClient } from './core/config/configureApollo';
-import theme from './theme';
 import './core/config/configureOthers';
 
 const store = configureStore();
@@ -13,11 +11,9 @@ const store = configureStore();
 export default function App() {
   return (
     <ApolloProvider store={store} client={apolloClient}>
-      <ThemeProvider theme={theme}>
-        <PortalProvider>
-          <RootView />
-        </PortalProvider>
-      </ThemeProvider>
+      <PortalProvider>
+        <RootView />
+      </PortalProvider>
     </ApolloProvider>
   );
 }
