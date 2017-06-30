4734c5a3478c724d0d14db39e27fadf86c3204db
doomsower <K.Kuznetcov@gmail.com>
2017-06-30 19:34:49 +0300

    Guided Tour View

--- mobile-client/src/guide/DefaultOverlay.js
+++ mobile-client/src/guide/DefaultOverlay.js
@@ -1,13 +1,60 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Dimensions, StyleSheet, View } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   overlay: {
     ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'transparent',
+  },
+  hole: {
+    position: 'absolute',
     backgroundColor: 'rgba(0,255,0,0.5)',
+    left: (width - 100) / 2,
+    width: 100,
+    top: (height - 100) / 2,
+    height: 100,
+    borderRadius: 50,
   },
 });
 
-export const DefaultOverlay = () => (
-  <View style={styles.overlay} pointerEvents="none" />
-);
+export class DefaultOverlay extends React.PureComponent {
+  onMoveShouldSetResponder = ({ nativeEvent }) => {
+    return this.isInside(nativeEvent);
+  };
+
+  onStartShouldSetResponder = ({ nativeEvent }) => {
+    return this.isInside(nativeEvent);
+  };
+
+  onMoveShouldSetResponderCapture = ({ nativeEvent }) => {
+    return this.isInside(nativeEvent);
+  };
+
+  onStartShouldSetResponderCapture = ({ nativeEvent }) => {
+    return this.isInside(nativeEvent);
+  };
+
+  isInside = ({ pageX, pageY }) => {
+    const dx = pageX - width / 2;
+    const dy = pageY - height / 2;
+    const result = Math.sqrt(dx * dx + dy * dy) < 50;
+    console.log(result);
+    return result;
+  };
+
+  render() {
+    return (
+      <View
+        style={styles.overlay}
+        onMoveShouldSetResponder={this.onMoveShouldSetResponder}
+        onStartShouldSetResponder={this.onStartShouldSetResponder}
+        onMoveShouldSetResponderCapture={this.onMoveShouldSetResponderCapture}
+        onStartShouldSetResponderCapture={this.onStartShouldSetResponderCapture}
+      >
+        <View style={styles.hole} pointerEvents="none" />
+      </View>
+    );
+  }
+}
--- mobile-client/src/guide/GuideOverlay.js
+++ mobile-client/src/guide/GuideOverlay.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Children, cloneElement } from 'react';
 import PropTypes from 'prop-types';
+import { NativeModules, findNodeHandle } from 'react-native';
 import { withGuideStep } from './withGuideStep';
 import { GuideStepPropType } from './PropTypes';
 
@@ -15,6 +16,11 @@ class GuideStepOverlay extends React.PureComponent {
     guideStep: GuideStepPropType.isRequired,
     background: PropTypes.element.isRequired,
   };
+
+  // constructor(props) {
+  //   super(props);
+  //   this._childWrapper = null;
+  // }
 
   componentDidMount() {
     const { background, guideStep } = this.props;
@@ -45,9 +51,35 @@ class GuideStepOverlay extends React.PureComponent {
       portalSet('guidedTourPortal', null);
     }
   }
+  //
+  // onLayout = () => {
+  //   if (this._childWrapper) {
+  //     const handle = findNodeHandle(this._childWrapper);
+  //     if (handle) {
+  //       NativeModules.UIManager.measure(handle, this.onChildMeasured);
+  //     }
+  //   }
+  // };
+  //
+  // onChildMeasured = (_x, _y, width, height, x, y) => {
+  //   // console.log('Child measured', width, height, x, y);
+  //   this.setState({ measured: true, layout: { x, y, width, height } });
+  // };
+  //
+  // setChildWrapperRef = (ref) => {
+  //   this._childWrapper = ref;
+  //   this.onLayout();
+  // };
 
   render() {
     return this.props.children;
+    // return cloneElement(
+    //   Children.only(this.props.children),
+    //   {
+    //     onLayout: this.onLayout,
+    //     ref: this.setChildWrapperRef,
+    //   },
+    // );
   }
 
 }
