b68831531ed67716635784b43cee15cf687a739a
doomsower <K.Kuznetcov@gmail.com>
2017-08-19 21:29:30 +0300

    Added wml rm

--- init_wml.sh
+++ init_wml.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+wml rm 3
+wml rm 2
+wml rm 1
+wml rm 0
+
 wml add commons web-client/src/commons
 wml add commons mobile-client/src/commons
 wml add commons backend/imports/commons
