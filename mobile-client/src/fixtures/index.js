99486cbd3309326d53bb9eb64a95af63baad25b2
doomsower <K.Kuznetcov@gmail.com>
2017-12-09 17:01:47 +0300

    Translate faq to russian

--- mobile-client/src/fixtures/index.js
+++
@@ -1,9 +0,0 @@
-import termsAndConditions from './terms_and_conditions';
-import privacyPolicy from './privacy_policy';
-import faq from './faq';
-
-export default {
-  termsAndConditions,
-  privacyPolicy,
-  faq,
-};
