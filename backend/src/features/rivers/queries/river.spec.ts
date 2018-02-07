5196eac9fc509abee565610f376defb66bb7c043
doomsower <K.Kuznetcov@gmail.com>
2018-02-07 21:12:07 +0300

    Added river tests

--- backend/src/features/rivers/queries/river.spec.ts
+++ backend/src/features/rivers/queries/river.spec.ts
@@ -6,41 +6,61 @@ beforeEach(holdTransaction);
 afterEach(rollbackTransaction);
 
 const query = `
-  query riverDetails($id: ID, $language: String){
+  query river($id: ID, $language: String){
     river(id: $id, language: $language) {
       id
       language
       name
-      description
-      season
-      seasonNumeric
-      bounds
-      hidden
-      createdAt
-      updatedAt
-      pois {
+      altNames
+      region {
         id
         language
         name
-        description
-        kind
-        coordinates
       }
+      sections {
+        nodes {
+          id
+          language
+          name
+        }
+        count
+      }
+      createdAt
+      updatedAt
     }
   }
 `;
 
 it('should return river', async () => {
+  const result = await runQuery(query, { id: 'a8416664-bfe3-11e7-abc4-cec278b6b50a' }, userContext);
+  expect(result.errors).toBeUndefined();
+  const river = result.data!.river;
+  expect(noTimestamps(river)).toMatchSnapshot();
 });
 
 it('should return null when id not specified', async () => {
+  const result = await runQuery(query, {}, userContext);
+  expect(result.errors).toBeUndefined();
+  expect(result.data).toBeDefined();
+  expect(result.data!.river).toBeNull();
 });
 
 it('should be able to specify language', async () => {
+  const result = await runQuery(
+    query,
+    { id: 'd4396dac-d528-11e7-9296-cec278b6b50a', language: 'ru' },
+    userContext,
+  );
+  expect(result.data!.river.name).toBe('Шоа');
 });
 
 it('should be able to get basic attributes without translation', async () => {
+  const result = await runQuery(query, { id: 'a8416664-bfe3-11e7-abc4-cec278b6b50a', language: 'pt' });
+  expect(result.errors).toBeUndefined();
+  expect(result).toHaveProperty('data.river.name', 'Not translated');
 });
 
-it('should get sections count', async () => {
+it('should get sections', async () => {
+  const result = await runQuery(query, { id: 'a8416664-bfe3-11e7-abc4-cec278b6b50a' }, userContext);
+  expect(result.data!.river.sections).toMatchSnapshot();
 });
