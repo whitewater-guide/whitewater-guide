5c46c0c69e9b9b434181e9027ab9b965348860ff
doomsower <K.Kuznetcov@gmail.com>
2018-03-03 23:52:53 +0300

    Some minio tests

--- packages/backend/src/minio/utils.test.ts
+++ packages/backend/src/minio/utils.test.ts
@@ -1,9 +1,25 @@
-import { MINIO_URL } from './buckets';
-import { getKeyFromImage } from './utils';
+import { getTempPostPolicy } from './utils';
 
-describe('getKeyFromImage', () => {
-  it('key should contain slash', () => {
-    const url = `${MINIO_URL}/temp/aaa.jpg`;
-    expect(getKeyFromImage(url, 'temp')).toBe('temp/aaa.jpg');
+describe('getTempPostPolicy', () => {
+  it('should return correct postURL and formData', async () => {
+    const result = await getTempPostPolicy();
+    expect(result).toEqual({
+      formData: {
+        'Content-Type': 'image/*',
+        'bucket': 'temp',
+        'policy': expect.any(String),
+        'x-amz-algorithm': 'AWS4-HMAC-SHA256',
+        'x-amz-credential': expect.any(String),
+        'x-amz-date': expect.stringMatching(/[0-9TZ]+/),
+        'x-amz-signature': expect.any(String),
+      },
+      postURL: 'http://localhost:6001/temp',
+    });
+  });
+});
+
+describe('moveTempImage', () => {
+  it.skip('should move files between buckets', async () => {
+
   });
 });
