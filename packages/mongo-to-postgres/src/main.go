c0c6a6617d5261b96e521ac88636d9c6d83322a9
doomsower <K.Kuznetcov@gmail.com>
2018-03-07 23:26:04 +0300

    Migration - tags

--- packages/mongo-to-postgres/src/main.go
+++ packages/mongo-to-postgres/src/main.go
@@ -4,6 +4,10 @@ import (
   "github.com/globalsign/mgo"
   "os"
   "fmt"
+  _ "github.com/lib/pq"
+  "database/sql"
+  "strings"
+  "github.com/lib/pq"
 )
 
 func main() {
@@ -13,20 +17,61 @@ func main() {
     fmt.Fprintf(os.Stderr, "Couldn't connect to mongo: %s", err.Error())
     os.Exit(1)
   }
-  db := session.DB("wwdb")
-  gaugesCollection := db.C("gauges")
+  mongo := session.DB("wwdb")
 
-  var gauge Gauge
-
-  iGauges := gaugesCollection.Find(nil).Iter()
-  for iGauges.Next(&gauge) {
-    // fmt.Printf("Next gauge: %v", gauge)
-    if gauge.RequestParams != nil {
-      fmt.Printf("Next gauge: %v", gauge)
-    }
+  pgConnStr := fmt.Sprintf(
+    "postgres://postgres:%s@localhost/%s?sslmode=disable",
+    os.Getenv("PGPASSWORD"),
+    os.Getenv("POSTGRES_DB"),
+  )
+  pg, err := sql.Open("postgres", pgConnStr)
+  if err != nil {
+    fmt.Fprintf(os.Stderr, "Couldn't connect to postgres: %s", err.Error())
+    os.Exit(1)
   }
-  if err := iGauges.Close(); err != nil {
-    fmt.Fprintf(os.Stderr, "Couldn't close users iterator: %s", err.Error())
+
+  clearPg(pg)
+
+  tags := insertTags(mongo, pg);
+
+  fmt.Println(tags)
+}
+
+func clearPg(pg *sql.DB) {
+  tables := []string{
+    "sources_regions",
+    "logins",
+    "users",
+    "gauges",
+    "gauges_translations",
+    "sections",
+    "sections_translations",
+    "sections_points",
+    "sections_tags",
+    "sections_media",
+    "sources",
+    "sources_translations",
+    "regions",
+    "regions_translations",
+    "regions_points",
+    "points",
+    "points_translations",
+    "rivers",
+    "rivers_translations",
+    "tags",
+    "tags_translations",
+    "measurements",
+    "media",
+    "media_translations",
+  }
+  for i, tbl := range tables {
+    tables[i] = pq.QuoteIdentifier(tbl)
+  }
+  q := fmt.Sprintf("TRUNCATE %s CASCADE", strings.Join(tables, ", "))
+  fmt.Println(q)
+
+  if _, err := pg.Query(q); err != nil {
+    fmt.Fprintf(os.Stderr, "Couldn't clear postgres before migrating: %s", err.Error())
     os.Exit(1)
   }
 }
--- packages/mongo-to-postgres/src/tags.go
+++ packages/mongo-to-postgres/src/tags.go
@@ -1,9 +1,51 @@
 package main
 
-import "github.com/globalsign/mgo/bson"
+import (
+  "github.com/globalsign/mgo/bson"
+  "github.com/globalsign/mgo"
+  "fmt"
+  "os"
+  "database/sql"
+)
 
 type Tag struct {
   ID   bson.ObjectId `bson:"_id"`
   Slug string        `bson:"slug"`
   Name string        `bson:"name"`
+}
+
+func insertTags(db *mgo.Database, pg *sql.DB) map[bson.ObjectId]string {
+  collections := map[string]string{
+    "hazard_tags":   "hazards",
+    "kayaking_tags": "kayaking",
+    "supply_tags":   "supply",
+    "misc_tags":     "misc",
+  }
+  var tagIds = make(map[bson.ObjectId]string)
+  var tag Tag
+  for cName, cat := range collections {
+    collection := db.C(cName)
+    iter := collection.Find(nil).Iter()
+    for iter.Next(&tag) {
+      _, err := pg.Query(`INSERT INTO tags(id, category) VALUES($1, $2) RETURNING id`, tag.Slug, cat)
+      if err != nil {
+        fmt.Fprintf(os.Stderr, "Couldn't insert tag %v: %s", tag, err.Error())
+      }
+      _, err = pg.Query(
+        "INSERT INTO tags_translations(tag_id, language, name) VALUES ($1, $2, $3)",
+        tag.Slug, "en", tag.Name,
+      )
+      if err != nil {
+        fmt.Fprintf(os.Stderr, "Couldn't insert translation %v: %s", tag, err.Error())
+      }
+      tagIds[tag.ID] = tag.Slug
+    }
+
+    if err := iter.Close(); err != nil {
+      fmt.Fprintf(os.Stderr, "Couldn't close tags iterator: %s", err.Error())
+      os.Exit(1)
+    }
+
+  }
+  return tagIds
 }
