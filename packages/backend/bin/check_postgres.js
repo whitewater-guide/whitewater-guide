16dc18632a371a609455d67c705ad1ca82e6ca8c
doomsower <K.Kuznetcov@gmail.com>
2018-03-08 23:56:38 +0300

    Better check-postgres script

--- packages/backend/bin/check_postgres.js
+++ packages/backend/bin/check_postgres.js
@@ -1,7 +1,19 @@
-#!/usr/bin/env node
-
 const pg = require('pg');
 const client = new pg.Client(`postgres://postgres:${process.env.PGPASSWORD}@db:5432/${process.env.POSTGRES_DB}`);
-client.query('SELECT NOW() AS "right_now"')
-  .then(() => process.exit(0))
-  .catch(() => process.exit(1));
+
+client.connect((err) => {
+  if (err) {
+    // console.log('[WATCHER] postgres is NOT ready');
+    process.exit(1);
+  } else {
+    client.query('SELECT NOW() AS "right_now"', (err, res) => {
+      if (err) {
+        // console.log('[WATCHER] postgres is NOT ready');
+        process.exit(1);
+      } else {
+        // console.log('[WATCHER] postgres is ready', res);
+        process.exit(0);
+      }
+    });
+  }
+});
