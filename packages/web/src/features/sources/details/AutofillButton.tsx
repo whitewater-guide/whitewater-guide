a1e929acb428fd44b02adff0e66166e4e23b9c28
doomsower <K.Kuznetcov@gmail.com>
2018-03-19 10:43:37 +0300

    [web] Fix react warnings about sourceId leaking into DOM

--- packages/web/src/features/sources/details/AutofillButton.tsx
+++ packages/web/src/features/sources/details/AutofillButton.tsx
@@ -2,6 +2,7 @@ import gql from 'graphql-tag';
 import { FlatButton, FlatButtonProps } from 'material-ui';
 import * as React from 'react';
 import { graphql } from 'react-apollo';
+import { compose, mapProps } from 'recompose';
 import { emitter, POKE_TABLES } from '../../../utils';
 
 const AUTOFILL_MUTATION = gql`
@@ -16,21 +17,24 @@ const AUTOFILL_MUTATION = gql`
 
 type OuterProps = Partial<FlatButtonProps> & { sourceId: string };
 
-const container = graphql<{}, OuterProps, FlatButtonProps>(
-  AUTOFILL_MUTATION,
-  {
-    options: {
-      refetchQueries: ['listGauges'],
+const container = compose<FlatButtonProps, OuterProps>(
+  graphql<{}, OuterProps, FlatButtonProps>(
+    AUTOFILL_MUTATION,
+    {
+      options: {
+        refetchQueries: ['listGauges'],
+      },
+      props: ({ mutate, ownProps: { sourceId } }) => ({
+        onClick: () =>
+          mutate!({ sourceId } as any)
+            .catch(() => {/* Ignore -> error goes to global snackbar */})
+            .finally(() => {
+              emitter.emit(POKE_TABLES);
+            }),
+      }),
     },
-    props: ({ mutate, ownProps: { sourceId } }) => ({
-      onClick: () =>
-        mutate!({ sourceId } as any)
-          .catch(() => {/* Ignore -> error goes to global snackbar */})
-          .finally(() => {
-            emitter.emit(POKE_TABLES);
-          }),
-    }),
-  },
+  ),
+  mapProps(({ sourceId, ...props }) => ({ ...props })),
 );
 
 const AutofillButton = container(FlatButton);
--- packages/web/src/features/sources/details/GenerateScheduleButton.tsx
+++ packages/web/src/features/sources/details/GenerateScheduleButton.tsx
@@ -2,6 +2,7 @@ import gql from 'graphql-tag';
 import { FlatButton, FlatButtonProps } from 'material-ui';
 import * as React from 'react';
 import { graphql } from 'react-apollo';
+import { compose, mapProps } from 'recompose';
 import { emitter, POKE_TABLES } from '../../../utils';
 
 const GENERATE_SOURCE_SCHEDULE = gql`
@@ -16,18 +17,21 @@ const GENERATE_SOURCE_SCHEDULE = gql`
 
 type OuterProps = Partial<FlatButtonProps> & { sourceId: string };
 
-const container = graphql<{}, OuterProps, FlatButtonProps>(
-  GENERATE_SOURCE_SCHEDULE,
-  {
-    props: ({ mutate, ownProps: { sourceId } }) => ({
-      onClick: () =>
-        mutate!({ sourceId } as any)
-          .catch(() => {/* Ignore -> error goes to global snackbar */})
-          .finally(() => {
-            emitter.emit(POKE_TABLES);
-          }),
-    }),
-  },
+const container = compose<FlatButtonProps, OuterProps>(
+  graphql<{}, OuterProps, FlatButtonProps>(
+    GENERATE_SOURCE_SCHEDULE,
+    {
+      props: ({ mutate, ownProps: { sourceId } }) => ({
+        onClick: () =>
+          mutate!({ sourceId } as any)
+            .catch(() => {/* Ignore -> error goes to global snackbar */})
+            .finally(() => {
+              emitter.emit(POKE_TABLES);
+            }),
+      }),
+    },
+  ),
+  mapProps(({ sourceId, ...props }) => ({ ...props })),
 );
 
 const GenerateScheduleButton = container(FlatButton);
