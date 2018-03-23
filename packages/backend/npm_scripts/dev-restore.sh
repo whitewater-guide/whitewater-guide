c225025c8f2d7d89350829d4e6d830fd4d38fc22
doomsower <K.Kuznetcov@gmail.com>
2018-03-23 18:59:15 +0300

    Fix typo

--- packages/backend/npm_scripts/dev-restore.sh
+++ packages/backend/npm_scripts/dev-restore.sh
@@ -2,4 +2,4 @@
 
 export $(cat ../../build/.env.development | grep POSTGRES_DB | xargs)
 # sed removes all comments and then removes all statements before alter
-docker exec -i ww-db pg_restore --username=postgres --dbname=$POSTGRES_DB < src/seeds/development/dymp.sql
+docker exec -i ww-db pg_restore --username=postgres --dbname=$POSTGRES_DB < src/seeds/development/dump.sql
