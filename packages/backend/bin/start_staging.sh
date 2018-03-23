6f98a8e55a945e1643702e9c387b47ac88d98d62
doomsower <K.Kuznetcov@gmail.com>
2018-03-24 00:35:42 +0300

    [backend] Staging dockerfile

---
+++ packages/backend/bin/start_staging.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+export NODE_PATH=/opt/app/node_modules:$NODE_PATH
+# Wait for db
+until node /opt/bin/check_postgres.js; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+# Wait for minio
+until node /opt/bin/check_minio.js; do
+  >&2 echo "Minio is unavailable - sleeping"
+  sleep 1
+done
+
+pm2-runtime start pm2.production.json --web
