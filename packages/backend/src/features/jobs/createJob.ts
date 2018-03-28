ae7ffe183aec282bc64f343efcbb267dded6ab39
doomsower <K.Kuznetcov@gmail.com>
2018-03-28 21:02:41 +0300

    [web] shorten logs

--- packages/backend/src/features/jobs/createJob.ts
+++ packages/backend/src/features/jobs/createJob.ts
@@ -3,11 +3,11 @@ import { execScript, ScriptCommand } from '../scripts';
 import { SourceRaw } from '../sources';
 import logger from './logger';
 
-export const createJob = (source: SourceRaw, gauge?: GaugeRaw) => async () => {
+export const createJob = ({ script }: SourceRaw, gauge?: GaugeRaw) => async () => {
   try {
     const { data, success, error } = await execScript<number>({
       command: ScriptCommand.HARVEST,
-      script: source.script,
+      script,
       code: gauge ? gauge.code : '',
       extras: gauge ? gauge.request_params : {},
     });
@@ -16,8 +16,7 @@ export const createJob = (source: SourceRaw, gauge?: GaugeRaw) => async () => {
         logger.warn({
           msg: 'Harvest returned 0 measurements',
           error,
-          source: source.id,
-          script: source.script,
+          script,
           gauge: gauge && gauge.code,
         });
       }
@@ -25,8 +24,7 @@ export const createJob = (source: SourceRaw, gauge?: GaugeRaw) => async () => {
       logger.error({
         msg: 'Harvest failed',
         error,
-        source: source.id,
-        script: source.script,
+        script,
         gauge: gauge && gauge.code,
       });
     }
@@ -34,8 +32,7 @@ export const createJob = (source: SourceRaw, gauge?: GaugeRaw) => async () => {
     logger.error({
       msg: 'Failed to run worker',
       error: err.message,
-      source: source.id,
-      script: source.script,
+      script,
       gauge: gauge && gauge.code,
     });
   }
