216ddb1226757233b881c3f14c1544b41ce12774
doomsower <K.Kuznetcov@gmail.com>
2018-03-31 01:24:11 +0300

    [build] Add `docker-compose down` to dev:start on Ctrl+C

--- build/scripts/dev_start.js
+++ build/scripts/dev_start.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const { spawn } = require('child_process');
+const { spawn, spawnSync } = require('child_process');
 const generateStackFile = require('./src/generateStackFile');
 const setupEnv = require('./src/setupEnv');
 
@@ -8,6 +8,10 @@ const CONFIG_NAME = 'development';
 async function devStart() {
   setupEnv(CONFIG_NAME);
   const stackFile = await generateStackFile(CONFIG_NAME);
+  process.on('SIGINT', () => {
+    console.log('Terminating dev stack');
+    spawnSync('docker-compose', ['-f', stackFile, 'down'], { stdio: 'inherit', shell: true });
+  });
   // Temporary do not use docker-sync
   // spawn('docker-sync-stack', ['start'], { stdio: 'inherit', shell: true });
   spawn('docker-compose', ['-f', stackFile, 'up'], { stdio: 'inherit', shell: true });
