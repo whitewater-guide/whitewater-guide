1a581a402cdee3ec269ae53aae5f313f13ebf2bd
doomsower <K.Kuznetcov@gmail.com>
2018-04-10 18:51:18 +0300

    [mobile] Add env files

--- build/scripts/wml_start.sh
+++ build/scripts/wml_start.sh
@@ -2,6 +2,8 @@
 
 watchman watch-del-all .
 
+./node_modules/.bin/wml rm 5
+./node_modules/.bin/wml rm 4
 ./node_modules/.bin/wml rm 3
 ./node_modules/.bin/wml rm 2
 ./node_modules/.bin/wml rm 1
@@ -11,5 +13,7 @@ watchman watch-del-all .
 ./node_modules/.bin/wml add packages/commons/src packages/backend/src/ww-commons
 ./node_modules/.bin/wml add packages/commons/src packages/web/src/ww-commons
 ./node_modules/.bin/wml add packages/clients/src packages/web/src/ww-clients
+./node_modules/.bin/wml add packages/commons/src packages/mobile/src/ww-commons
+./node_modules/.bin/wml add packages/clients/src packages/mobile/src/ww-clients
 
 ./node_modules/.bin/wml start
