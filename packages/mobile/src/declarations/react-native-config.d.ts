aa0fee660cb779924aabfc8b5d398a22fc2cd90d
doomsower <K.Kuznetcov@gmail.com>
2018-04-10 20:59:25 +0300

    [mobile] Make app build on android

--- packages/mobile/src/declarations/react-native-config.d.ts
+++ packages/mobile/src/declarations/react-native-config.d.ts
@@ -2,6 +2,7 @@ declare module 'react-native-config' {
   interface IConfig {
     GOOGLE_API_KEY: string;
     BACKEND: string;
+    FACEBOOK_APP_ID: string;
   }
 
   declare const Config: IConfig;
