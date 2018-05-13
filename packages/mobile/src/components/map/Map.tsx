acc826fbc51f67032712cc80a58f88207ba39ce6
doomsower <K.Kuznetcov@gmail.com>
2018-05-13 20:18:40 +0300

    [mobile] Fix initial region for maps

--- packages/mobile/src/components/map/Map.tsx
+++ packages/mobile/src/components/map/Map.tsx
@@ -2,28 +2,17 @@ import React from 'react';
 import { LayoutChangeEvent, StyleSheet, View } from 'react-native';
 import RNMapView, { Region as MapsRegion } from 'react-native-maps';
 import { MapBody, MapComponentProps, MapProps } from '../../ww-clients/features/maps';
-import { getBBox, getBoundsZoomLevel } from '../../ww-clients/utils';
+import { computeDistanceBetween, computeOffset, getBBox, getBoundsZoomLevel } from '../../ww-clients/utils';
 import { Coordinate } from '../../ww-commons/features/points';
 import { SimplePOI } from './SimplePOI';
 import { SimpleSection } from './SimpleSection';
 
 export class MapView extends React.PureComponent<MapComponentProps> {
-  _initialRegion: MapsRegion;
+  _map: RNMapView | null;
+  _mapLaidOut: boolean = false;
+  _userLocationSet: boolean = false;
   _bounds: Coordinate[];
   _dimensions: { width: number, height: number };
-
-  constructor(props: MapComponentProps) {
-    super(props);
-    const [minLng, maxLng, minLat, maxLat] = getBBox(
-      props.initialBounds || props.contentBounds || [[-180, -90], [180, 90]],
-    );
-    this._initialRegion = {
-      latitude: (maxLat + minLat) / 2,
-      longitude: (maxLng + minLng) / 2,
-      latitudeDelta: (maxLat - minLat) / 2,
-      longitudeDelta: (maxLng - minLng) / 2,
-    };
-  }
 
   onDeselect = () => {
     const { onSectionSelected, onPOISelected } = this.props;
@@ -44,38 +33,46 @@ export class MapView extends React.PureComponent<MapComponentProps> {
   };
 
   onMapLayout = ({ nativeEvent: { layout: { width, height } } }: LayoutChangeEvent) => {
-    // if (this._mapLaidOut || !this._mapView) {
-    //   return;
-    // }
-    // this._mapLaidOut = true;
     this._dimensions = { width, height };
-    // const { initialBounds, contentBounds } = this.props;
-    // if (initialBounds) {
-    //   if (!this._requestedGeolocation) {
-    //     this._requestedGeolocation = true;
-    //     navigator.geolocation.getCurrentPosition(
-    //       this.onGeolocationSuccess,
-    //       () => {},
-    //       { enableHighAccuracy: true, timeout: 5000, maximumAge: 60000 },
-    //     );
-    //   }
-    // } else if (contentBounds) {
-    //   // Without this timeout, fitToCoordinates sometimes does nothing
-    //   // Maybe this PR can solve things: https://github.com/airbnb/react-native-maps/pull/1369
-    //   setTimeout(() => {
-    //     const ir = this._initialRegion;
-    //     this._mapView.fitToCoordinates(
-    //       [
-    //         { latitude: ir.latitude - ir.latitudeDelta, longitude: ir.longitude - ir.longitudeDelta },
-    //         { latitude: ir.latitude + ir.latitudeDelta, longitude: ir.longitude + ir.longitudeDelta },
-    //       ],
-    //       {
-    //         edgePadding: { left: 16, right: 16, top: 16, bottom: 16 },
-    //         animated: false,
-    //       },
-    //     );
-    //   }, 200);
-    // }
+    if (this._map && !this._mapLaidOut) {
+      this._mapLaidOut = true;
+      this._map.fitToCoordinates(
+        this.props.initialBounds!.map(([ longitude, latitude]) => ({ longitude, latitude })),
+        {
+          edgePadding: { top: 20, left: 20, right: 20, bottom: 20 },
+          animated: false,
+        },
+      );
+    }
+  };
+
+  onUserLocationChange = (evt: any) => {
+    if (this._userLocationSet) {
+      return;
+    }
+    this._userLocationSet = true;
+    const { latitude, longitude } = evt.nativeEvent.coordinate;
+    const [minLng, maxLng, minLat, maxLat] = getBBox(this.props.initialBounds);
+    if (this._map && latitude >= minLat && latitude <= maxLat && longitude >= minLng && longitude <= maxLng) {
+      if (computeDistanceBetween([minLng, minLat], [maxLng, maxLat]) < 150) {
+        this._map.animateToCoordinate({ latitude, longitude }, 300);
+      } else {
+        const corner = computeOffset({ latitude, longitude }, 100, -45);
+        this._map.animateToRegion(
+          {
+            latitude,
+            longitude,
+            latitudeDelta: Math.abs(latitude - corner.latitude),
+            longitudeDelta: Math.abs(longitude - corner.longitude),
+          },
+          300,
+        );
+      }
+    }
+  };
+
+  setMapRef = (ref: RNMapView) => {
+    this._map = ref;
   };
 
   render() {
@@ -86,10 +83,10 @@ export class MapView extends React.PureComponent<MapComponentProps> {
           showsMyLocationButton
           showsCompass
           showsScale
+          ref={this.setMapRef}
           rotateEnabled={false}
           pitchEnabled={false}
           toolbarEnabled={false}
-          initialRegion={this._initialRegion}
           style={StyleSheet.absoluteFill}
           provider="google"
           onPress={this.onDeselect}
@@ -97,6 +94,7 @@ export class MapView extends React.PureComponent<MapComponentProps> {
           onRegionChange={this.onRegionChange}
           onRegionChangeComplete={this.onRegionChange}
           onLayout={this.onMapLayout}
+          onUserLocationChange={this.onUserLocationChange}
         >
           {this.props.children}
         </RNMapView>
