e7f20d16b0a01581b3dd98b93c4814dacb4c73d7
doomsower <K.Kuznetcov@gmail.com>
2018-05-17 15:34:21 +0300

    [mobile] Imporove selected section panel ui, closes #206

--- packages/mobile/src/components/map/SelectedSectionView.tsx
+++ packages/mobile/src/components/map/SelectedSectionView.tsx
@@ -9,10 +9,12 @@ import { Button, Paragraph, Subheading } from 'react-native-paper';
 import Svg, { Path } from 'react-native-svg';
 import { NavigationInjectedProps, withNavigation } from 'react-navigation';
 import { compose } from 'recompose';
+import sections from '../../../../backend/src/features/sections/queries/sections';
 import { WithT } from '../../i18n';
 import theme from '../../theme';
 import { SelectedSectionViewProps } from '../../ww-clients/features/maps';
 import { stringifySeason } from '../../ww-clients/utils';
+import { Section } from '../../ww-commons/features/sections';
 import { DifficultyThumb } from '../DifficultyThumb';
 import { Icon } from '../Icon';
 import { NAVIGATE_BUTTON_WIDTH } from '../NavigateButton';
@@ -39,36 +41,29 @@ const styles = StyleSheet.create({
     width: 80,
     paddingTop: 2,
   },
-  col1: {
+  col: {
     alignSelf: 'stretch',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    overflow: 'hidden',
+    flex: 1,
+    height: theme.rowHeight,
+  },
+  col1: {
     paddingRight: theme.margin.half,
-    width: 2 * NAVIGATE_BUTTON_WIDTH - theme.margin.single,
     borderRightWidth: StyleSheet.hairlineWidth,
     borderRightColor: theme.colors.border,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    overflow: 'hidden',
   },
   col2: {
-    alignSelf: 'stretch',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    overflow: 'hidden',
     paddingHorizontal: theme.margin.half,
-    flex: 1,
   },
   col3: {
-    alignSelf: 'stretch',
     paddingLeft: theme.margin.half,
-    width: 2 * NAVIGATE_BUTTON_WIDTH - theme.margin.single,
     borderLeftWidth: StyleSheet.hairlineWidth,
     borderLeftColor: theme.colors.border,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    overflow: 'hidden',
+    width: 2 * NAVIGATE_BUTTON_WIDTH - theme.margin.single,
+    flex: undefined,
   },
   colText: {
     flex: 1,
@@ -89,7 +84,24 @@ const styles = StyleSheet.create({
 
 type Props = SelectedSectionViewProps & WithT & NavigationInjectedProps;
 
-class SelectedSectionViewInternal extends React.Component<Props> {
+interface State {
+  section: Section | null;
+}
+
+class SelectedSectionViewInternal extends React.Component<Props, State> {
+
+  static getDerivedStateFromProps(nextProps: Props, prevState: State): State {
+    // Keep section as state to prevent flash of empty content before panel hides
+    if (nextProps.selectedSection) {
+      return { ...prevState, section: nextProps.selectedSection };
+    }
+    return prevState;
+  }
+
+  constructor(props: Props) {
+    super(props);
+    this.state = { section: props.selectedSection };
+  }
 
   shouldComponentUpdate(next: Props) {
     const oldId = this.props.selectedSection && this.props.selectedSection.id;
@@ -105,7 +117,7 @@ class SelectedSectionViewInternal extends React.Component<Props> {
   };
 
   renderHeader = () => {
-    const { selectedSection: section } = this.props;
+    const { section } = this.state;
     return (
       <View style={styles.header}>
         <View style={styles.body}>
@@ -129,7 +141,8 @@ class SelectedSectionViewInternal extends React.Component<Props> {
   };
 
   render() {
-    const { selectedSection: section, t } = this.props;
+    const { t } = this.props;
+    const { section } = this.state;
     const buttons = [
       { label: t('commons:putIn'), coordinates: get(section, 'putIn.coordinates', [0, 0]) },
       { label: t('commons:takeOut'), coordinates: get(section, 'takeOut.coordinates', [0, 0]) },
@@ -139,7 +152,7 @@ class SelectedSectionViewInternal extends React.Component<Props> {
       season = [
         capitalize(trim(stringifySeason(section.seasonNumeric, false))),
         trim(section.season),
-      ].join('\n');
+      ].join('\n').trim();
     }
     const seasonNumLines = season.includes('\n') ? 2 : 1;
     const duration = (section && section.duration) ? t('durations:' + section.duration) : t('commons:unknown');
@@ -149,12 +162,12 @@ class SelectedSectionViewInternal extends React.Component<Props> {
       <SelectedElementView
         renderHeader={this.renderHeader}
         buttons={buttons}
-        selected={!!section}
+        selected={!!this.props.selectedSection}
         onSectionSelected={this.props.onSectionSelected}
         onPOISelected={noop}
       >
         <View style={[styles.listItem, styles.listItemWithBorder]}>
-          <View style={styles.col1}>
+          <View style={[styles.col, styles.col1]}>
             <Svg width={24} height={24}>
               {/*tslint:disable-next-line*/}
               <Path fill={theme.colors.textMain} d="M6.5,8.11C5.61,8.11 4.89,7.39 4.89,6.5A1.61,1.61 0 0,1 6.5,4.89C7.39,4.89 8.11,5.61 8.11,6.5V6.5A1.61,1.61 0 0,1 6.5,8.11M6.5,2C4,2 2,4 2,6.5C2,9.87 6.5,14.86 6.5,14.86C6.5,14.86 11,9.87 11,6.5C11,4 9,2 6.5,2M17.5,8.11A1.61,1.61 0 0,1 15.89,6.5C15.89,5.61 16.61,4.89 17.5,4.89C18.39,4.89 19.11,5.61 19.11,6.5A1.61,1.61 0 0,1 17.5,8.11M17.5,2C15,2 13,4 13,6.5C13,9.87 17.5,14.86 17.5,14.86C17.5,14.86 22,9.87 22,6.5C22,4 20,2 17.5,2M17.5,16C16.23,16 15.1,16.8 14.68,18H9.32C8.77,16.44 7.05,15.62 5.5,16.17C3.93,16.72 3.11,18.44 3.66,20C4.22,21.56 5.93,22.38 7.5,21.83C8.35,21.53 9,20.85 9.32,20H14.69C15.24,21.56 16.96,22.38 18.5,21.83C20.08,21.28 20.9,19.56 20.35,18C19.92,16.8 18.78,16 17.5,16V16M17.5,20.5A1.5,1.5 0 0,1 16,19A1.5,1.5 0 0,1 17.5,17.5A1.5,1.5 0 0,1 19,19A1.5,1.5 0 0,1 17.5,20.5Z" />
@@ -163,13 +176,13 @@ class SelectedSectionViewInternal extends React.Component<Props> {
               {distance ? `${distance} ${t('commons:km')}` : t('commons:unknown')}
             </Paragraph>
           </View>
-          <View style={styles.col2}>
+          <View style={[styles.col, styles.col2]}>
             <Icon icon="trending-down" color={theme.colors.textMain} size={24} />
             <Paragraph  style={styles.colText}>
               {drop ? `${drop} ${t('commons:m')}` : t('commons:unknown')}
             </Paragraph>
           </View>
-          <View style={styles.col3}>
+          <View style={[styles.col, styles.col3]}>
             <Icon icon="clock" color={theme.colors.textMain} size={24} />
             <Paragraph style={styles.colText}>
               {duration}
