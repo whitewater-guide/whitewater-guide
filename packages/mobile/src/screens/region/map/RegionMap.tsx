cb8a4b9e66e457843581af3838344f4495d98cfe
doomsower <K.Kuznetcov@gmail.com>
2018-05-23 00:28:48 +0300

    [mobile] Track map errors

--- packages/mobile/src/screens/region/map/RegionMap.tsx
+++ packages/mobile/src/screens/region/map/RegionMap.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ErrorBoundary from 'react-error-boundary';
+import { ErrorBoundaryFallback } from '../../../components';
 import { getMapLayout, Map, SelectedPOIView, SelectedSectionView } from '../../../components/map';
+import { trackError } from '../../../core/errors';
 import { getMapView, MapProps } from '../../../ww-clients/features/maps';
 import { regionMapContainer } from '../../../ww-clients/features/regions/map';
 
 const View: React.ComponentType<MapProps> =
   getMapView<MapProps>(getMapLayout('region'), Map, SelectedSectionView, SelectedPOIView);
 
+const reportError = (error: Error, componentStack: string) =>
+  trackError('region_map', error, componentStack);
+
 const RegionMap: React.StatelessComponent<MapProps> = (props) => (
-  <ErrorBoundary>
+  <ErrorBoundary FallbackComponent={ErrorBoundaryFallback} onError={reportError}>
     <View {...props} />
   </ErrorBoundary>
 );
