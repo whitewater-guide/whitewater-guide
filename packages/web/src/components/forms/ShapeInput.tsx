2d6cac1e6b45366995d3ecb660070c23b7699230
doomsower <K.Kuznetcov@gmail.com>
2018-05-29 10:04:39 +0300

    [web] Add swap put-in and take-out button

--- packages/web/src/components/forms/ShapeInput.tsx
+++ packages/web/src/components/forms/ShapeInput.tsx
@@ -1,13 +1,27 @@
+import IconButton from 'material-ui/IconButton';
 import React from 'react';
 import { BaseFieldArrayProps, FieldArray, GenericFieldArray, WrappedFieldArrayProps } from 'redux-form';
 import { Styles } from '../../styles';
 import { computeDistanceBetween } from '../../ww-clients/utils';
 import { Coordinate3d } from '../../ww-commons';
 import { LatLonAltInput, LLAArrayField } from './LatLonAltInput';
+import Divider from 'material-ui/Divider';
 
 const styles: Styles = {
   container: {
     height: '100%',
+  },
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  lengthBlock: {
+    flex: 1,
+  },
+  divider: {
+    marginTop: 8,
+    marginBottom: 8,
   },
 };
 
@@ -19,10 +33,12 @@ class ShapeInputComponent extends React.Component<Props> {
     const points = this.props.fields.getAll() || [];
     let distance = 0;
     for (let i = 1; i < points.length; i += 1) {
-      distance += computeDistanceBetween(points[i - 1], points[i]);
+      try {
+        distance += computeDistanceBetween(points[i - 1], points[i]);
+      } catch (e) {}
     }
     return (
-      <div>
+      <div style={styles.lengthBlock}>
         <strong>Distance:</strong> {`${distance.toFixed(3)} km`}
       </div>
     );
@@ -32,11 +48,31 @@ class ShapeInputComponent extends React.Component<Props> {
     this.props.fields.push(value);
   };
 
+  onSwap = () => {
+    const { fields } = this.props;
+    const len = fields.length;
+    const half = Math.floor(len / 2);
+    for (let i = 0; i < half; i++) {
+      fields.swap(i, len - 1 - i);
+    }
+  };
+
   render() {
     const { fields } = this.props;
     return (
       <div style={styles.container}>
-        {this.renderLineLength()}
+        <div style={styles.header}>
+          {this.renderLineLength()}
+          <IconButton
+            disabled={fields.length < 2}
+            iconClassName="material-icons"
+            onClick={this.onSwap}
+            tooltip="Swap put-in and take-out"
+          >
+            swap_calls
+          </IconButton>
+        </div>
+        <Divider style={styles.divider} />
         {
           fields.map((name, index) => (
             <LLAArrayField key={index} name={name} index={index} fields={fields} />
