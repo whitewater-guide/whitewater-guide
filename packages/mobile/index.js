ed9eba143a3a2cc5a81a6f64e51d79539b7d7930
doomsower <K.Kuznetcov@gmail.com>
2018-05-31 21:03:17 +0300

    [mobile] Revert polyfill changes from previous commit, closes #240

--- packages/mobile/index.js
+++ packages/mobile/index.js
@@ -1,12 +1,5 @@
-// https://github.com/facebook/react-native/issues/15902#issuecomment-375521246
-// symbol polyfills
-global.Symbol = require('core-js/es6/symbol');
-require('core-js/fn/symbol/iterator');
-
-// collection fn polyfills
+import 'es6-symbol/implement';
 require('core-js/fn/map');
-require('core-js/fn/set');
-require('core-js/fn/array/find');
 
 import { AppRegistry } from 'react-native';
 import App from './src/App';
