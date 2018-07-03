41b03cc729c31d4f1555c87e410e97689fe97773
doomsower <K.Kuznetcov@gmail.com>
2018-07-03 15:12:30 +0300

    [build] Update script to download images

--- build/scripts/dev_download_images.sh
+++ build/scripts/dev_download_images.sh
@@ -5,5 +5,5 @@
 # So in dev environment images are available
 # It also compresses them so they can be built into image (for local, staging deploys)
 
-docker-machine scp -r -d wwguide:/mnt/dostorage/wwguide-uploads/images/ ./packages/minio/data/media
-tar czf ./packages/minio/images.tar.gz -C ./packages/minio/data media
+docker-machine scp -r -d ww-production:/var/minio/data/ ./packages/minio/data
+tar czf ./packages/minio/images.tar.gz -C ./packages/minio data
