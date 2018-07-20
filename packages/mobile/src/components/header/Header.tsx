4340ae9e79793310664fee2d82e0ea751e430594
doomsower <K.Kuznetcov@gmail.com>
2018-07-20 12:25:11 +0300

    [mobile] Fix header arrow

--- packages/mobile/src/components/header/Header.tsx
+++ packages/mobile/src/components/header/Header.tsx
@@ -25,7 +25,7 @@ class Header extends React.PureComponent<Props> {
         size: 36,
       },
       android: {
-        icon: 'arrow-back',
+        icon: 'arrow-left',
         size: 24,
       },
     });
