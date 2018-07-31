b9a0aecb9f337b3888453d4d1a4ef1c1b1e67f76
doomsower <K.Kuznetcov@gmail.com>
2018-07-31 13:01:04 +0300

    [commons] update deps

---
+++ packages/commons/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // globals: {
+  //   'ts-jest': {
+  //     isolatedModules: true
+  //   }
+  // },
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  testEnvironment: 'node',
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx)$',
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'json'
+  ]
+};
