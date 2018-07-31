894bf3ec3a52f87327c15cb2d87289d53d826aa7
doomsower <K.Kuznetcov@gmail.com>
2018-07-31 13:01:04 +0300

    fix(workers) More debug messages for chile

--- packages/workers/src/chile/loadXLS.go
+++ packages/workers/src/chile/loadXLS.go
@@ -8,11 +8,13 @@ import (
   log "github.com/sirupsen/logrus"
   "github.com/moul/http2curl"
   "encoding/base64"
+  "fmt"
 )
 
 const xlsUrl = "http://dgasatel.mop.cl/cons_det_instan_xls.asp"
 
 func loadXLS(code string, since int64) (string, error) {
+  fmt.Println("Load XLS", code, since)
   tz, err := time.LoadLocation("America/Santiago")
   if err != nil {
     return "", nil
@@ -23,7 +25,7 @@ func loadXLS(code string, since int64) (string, error) {
   }
   t := time.Now().In(tz)
   cookieErr := core.Client.EnsureCookie("http://dgasatel.mop.cl", "http://dgasatel.mop.cl")
-  html, req, err := core.Client.PostFormAsString(xlsUrl, url.Values{
+  values := url.Values{
     "accion":         {"refresca"},
     "chk_estacion1a": {code + "_1", code + "_12"},
     "chk_estacion1b": {""},
@@ -39,7 +41,8 @@ func loadXLS(code string, since int64) (string, error) {
     "fecha_ini":      {t.Format("02/01/2006")},
     "period":         {period},
     "tiporep":        {"I"},
-  })
+  }
+  html, req, err := core.Client.PostFormAsString(xlsUrl, values)
 
   // TEMPORARY CODE
   if strings.Index(html, "tabla para resultados numerados") == -1 {
@@ -50,6 +53,7 @@ func loadXLS(code string, since int64) (string, error) {
       "since":     since,
       "cookieErr": cookieErr != nil,
       "req":       encoded,
+      "values":    values.Encode(),
     }).Warn("missing data table in XLS response")
   }
 
