512b952ad04241386bbb6ac39bb9fd36f1c47ec3
doomsower <K.Kuznetcov@gmail.com>
2018-07-31 13:01:04 +0300

    fix(mobile): Move banner from section info to section description

--- packages/mobile/src/screens/section/guide/SectionGuideScreen.tsx
+++ packages/mobile/src/screens/section/guide/SectionGuideScreen.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { NavigationScreenComponent } from 'react-navigation';
 import { Icon, Screen } from '../../../components';
+import { RegionBanners } from '../../../features/banners';
 import { I18nText } from '../../../i18n';
 import theme from '../../../theme';
+import { BannerPlacement } from '../../../ww-commons';
 import { ScreenProps } from '../types';
 import SectionGuideView from './SectionGuideView';
 
 export const SectionGuideScreen: NavigationScreenComponent = ({ screenProps }) => (
   <Screen>
     <SectionGuideView section={(screenProps as ScreenProps).section} />
+    <RegionBanners placement={BannerPlacement.MOBILE_SECTION_DESCRIPTION} count={10} />
   </Screen>
 );
 
