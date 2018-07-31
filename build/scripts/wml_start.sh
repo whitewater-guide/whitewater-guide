8abcb3411454b6b8890210b3843dca4a1651cf9e
doomsower <K.Kuznetcov@gmail.com>
2018-07-31 13:01:04 +0300

    [build] Improve wml:start

--- build/scripts/wml_start.sh
+++ build/scripts/wml_start.sh
@@ -4,6 +4,16 @@ watchman watch-del-all .
 
 ./node_modules/.bin/wml rm all
 
+# Clean destinations because they might contain old files and files from other branches after switch
+rm -rf packages/clients/ww-commons
+rm -rf packages/backend/src/ww-commons
+rm -rf packages/boompromo/src/ww-commons
+rm -rf packages/boompromo/src/ww-clients
+rm -rf packages/web/src/ww-commons
+rm -rf packages/web/src/ww-clients
+rm -rf packages/mobile/src/ww-commons
+rm -rf packages/mobile/src/ww-clients
+
 ./node_modules/.bin/wml add packages/commons/src packages/clients/ww-commons
 ./node_modules/.bin/wml add packages/commons/src packages/backend/src/ww-commons
 ./node_modules/.bin/wml add packages/commons/src packages/boompromo/src/ww-commons
