e26dcdd44397640b18ea6c9833feada05505d40f
doomsower <K.Kuznetcov@gmail.com>
2019-01-18 14:48:27 +0300

    build(mobile): Update i18next

--- packages/mobile/src/i18n/I18nProvider.tsx
+++ packages/mobile/src/i18n/I18nProvider.tsx
@@ -12,53 +12,58 @@ interface Props {
   language?: string;
 }
 
-export class I18nProviderInternal extends React.PureComponent<Props> {
-  i18n: i18next.i18n;
+interface State {
+  ready: boolean;
+}
 
-  constructor(props: Props) {
-    super(props);
-    const language = props.language || RNLanguages.language || 'en';
-    this.i18n = i18next
-      .use(reactI18nextModule)
-      .init({
-        lng: (language).substr(0, 2),
-        fallbackLng: 'en',
-        whitelist: SUPPORTED_LANGUAGES,
-        interpolation: {
-          escapeValue: false, // not needed for react!!
-        },
-        react: {
-          nsMode: 'fallback',
-        },
-        resources: { en, ru },
-      });
-    moment.locale(this.i18n.languages[0]);
-    this.i18n.on('languageChanged', this.onLanguageChange);
+export class I18nProviderInternal extends React.PureComponent<Props, State> {
+  readonly state: State = { ready: false };
+
+  private _i18n: i18next.i18n = i18next.use(reactI18nextModule);
+
+  async componentDidMount() {
+    const language = this.props.language || RNLanguages.language || 'en';
+    const lng = (language).substr(0, 2);
+    await this._i18n.init({
+      lng,
+      fallbackLng: 'en',
+      whitelist: SUPPORTED_LANGUAGES,
+      interpolation: {
+        escapeValue: false, // not needed for react!!
+      },
+      react: {
+        nsMode: 'fallback',
+      },
+      resources: { en, ru },
+    });
+    moment.locale(lng);
+    this._i18n.on('languageChanged', this.onLanguageChange);
+    this.setState({ ready: true });
   }
 
   componentWillUnmount() {
-    this.i18n.off('languageChanged', this.onLanguageChange);
+    this._i18n.off('languageChanged', this.onLanguageChange);
   }
 
   componentWillReceiveProps(nextProps: Props) {
     if (this.props.language !== nextProps.language) {
       const language = (nextProps.language || RNLanguages.language || 'en').substr(0, 2);
-      this.i18n.changeLanguage(language);
+      this._i18n.changeLanguage(language);
     }
   }
 
   onLanguageChange = (language: string) => {
     if (language) {
-      moment.locale(this.i18n.languages[0]);
+      moment.locale(this._i18n.languages[0]);
     }
   };
 
   render() {
-    return this.props.children;
+    return this.state.ready ? this.props.children : null;
   }
 }
 
-export const I18nProvider: React.StatelessComponent = ({ children }) => (
+export const I18nProvider: React.FC = ({ children }) => (
   <MyProfileConsumer>
     {({ me }) => (
       <I18nProviderInternal language={me ? me.language : undefined}>
