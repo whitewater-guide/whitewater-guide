c15273f36d469d2360ca6e01ad09aa7e592a25cc
doomsower <K.Kuznetcov@gmail.com>
2019-02-04 01:34:41 +0300

    feat(backend): reanimate graphiql/playground
    related to #326

---
+++ services/backend/src/apollo/server/playground.ts
@@ -0,0 +1,71 @@
+import { PlaygroundConfig } from 'apollo-server';
+import { getIntrospectionQuery, graphql, GraphQLSchema } from 'graphql';
+import { FilterRootFields } from 'graphql-tools';
+// tslint:disable-next-line:no-submodule-imports
+import { RootFilter } from 'graphql-tools/dist/transforms/FilterRootFields';
+import FilterFieldsWithDirective from './FilterFieldsWithDirective';
+
+const rootFieldsFilter: RootFilter = (operation, fieldName, field) => {
+  if (operation === 'Mutation') {
+    return false;
+  }
+  const isAdmin =
+    field.astNode &&
+    field.astNode.directives &&
+    field.astNode.directives.some((d) => d.name.value === 'admin');
+  return !isAdmin;
+};
+
+const getPlaygroundSchema = (schema: GraphQLSchema) => {
+  const filterRootFields = new FilterRootFields(rootFieldsFilter);
+  const filterAdminFields = new FilterFieldsWithDirective('admin');
+  return filterAdminFields.transformSchema(
+    filterRootFields.transformSchema(schema),
+  );
+};
+
+export const getPlaygroundConfig = async (
+  schema: GraphQLSchema,
+): Promise<PlaygroundConfig> => {
+  const introspectionQuery = getIntrospectionQuery();
+  const playgroundSchema = getPlaygroundSchema(schema);
+  const { data } = await graphql(
+    playgroundSchema,
+    introspectionQuery,
+    undefined,
+  );
+  return {
+    schema: data,
+    tabs: [
+      {
+        name: 'Example: some regions',
+        endpoint: '',
+        query: `
+        query listRegions($page: Page) {
+          regions(page: $page) {
+            nodes {
+              id
+              name
+              premium
+              hasPremiumAccess
+              editable
+              sku
+              rivers {
+                count
+              }
+              gauges {
+                count
+              }
+              sections {
+                count
+              }
+            }
+            count
+          }
+        }
+       `,
+        variables: JSON.stringify({ page: { limit: 5 } }),
+      } as any,
+    ],
+  };
+};
--- services/backend/src/apollo/server/server.ts
+++ services/backend/src/apollo/server/server.ts
@@ -4,17 +4,21 @@ import * as Koa from 'koa';
 import { newContext } from '../context';
 import { formatError } from '../formatError';
 import { logger } from '../logger';
+import { getPlaygroundConfig } from './playground';
 import { graphqlRouter } from './router';
 import { getSchema } from './schema';
 
 export const createApolloServer = async (app: Koa) => {
   const schema = await getSchema();
+  const playground = await getPlaygroundConfig(schema);
   const server = new ApolloServer({
     schema,
     context: newContext,
     dataSources: createConnectors,
     formatError,
     debug: process.env.NODE_ENV === 'development',
+    introspection: false,
+    playground,
   });
 
   server.applyMiddleware({
