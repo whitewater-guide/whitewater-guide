9e7dca654c22747f5480f90e208ea2dae7a5ade8
doomsower <K.Kuznetcov@gmail.com>
2019-02-04 20:22:04 +0300

    feat(commons): overload withZeroAlt
    allow withZeroAlt to accept either array or single coordinate

--- packages/commons/src/features/points/types.ts
+++ packages/commons/src/features/points/types.ts
@@ -2,10 +2,21 @@ export type Coordinate2d = [number, number];
 export type Coordinate3d = [number, number, number];
 export type Coordinate = Coordinate2d | Coordinate3d;
 
-export function withZeroAlt(coordinates: Coordinate[]): Coordinate3d[] {
-  return coordinates.map(
-    ([lng, lat, alt]: any) => [lng, lat, alt || 0] as Coordinate3d,
-  );
+function isCoordinate(value: any): value is Coordinate {
+  return Array.isArray(value) && value.every((i) => typeof i === 'number');
+}
+
+export function withZeroAlt(coordinates: Coordinate): Coordinate3d;
+export function withZeroAlt(coordinates: Coordinate[]): Coordinate3d[];
+export function withZeroAlt(coordinates: any): any {
+  if (isCoordinate(coordinates)) {
+    const [ln, la, al] = coordinates as any;
+    return [ln, la, al || 0] as Coordinate3d;
+  } else {
+    return coordinates.map(
+      ([lng, lat, alt]: any) => [lng, lat, alt || 0] as Coordinate3d,
+    );
+  }
 }
 
 export interface Point {
