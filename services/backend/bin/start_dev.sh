a3e9273ad985925d23043fde2ab7884411c60838
doomsower <K.Kuznetcov@gmail.com>
2019-02-05 20:06:09 +0300

    build(backend): mount @whitewater-guide/commons in dev docker-compose

--- services/backend/bin/start_dev.sh
+++ services/backend/bin/start_dev.sh
@@ -11,7 +11,7 @@ fi
 # TODO: remove this (and from Dockerfile as well) when PM2 gets update: https://github.com/Unitech/pm2/issues/2629
 SHELL=/bin/sh chokidar package.json -p --poll-interval 300 -c yarn --production=true &
 
-export NODE_PATH=/opt/app/node_modules:$NODE_PATH
+export NODE_PATH=/opt/app/node_modules:/opt/app/lerna_modules:$NODE_PATH
 # Wait for db
 until node /opt/bin/check_postgres.js; do
   >&2 echo "Postgres is unavailable - sleeping"
