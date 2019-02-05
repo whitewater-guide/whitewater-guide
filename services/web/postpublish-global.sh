18af604a142cb69673668ad7913c0f22ae4db051
doomsower <K.Kuznetcov@gmail.com>
2019-02-05 11:16:31 +0300

    build(web): better sentry publish
    do not publish on sentry if docker publish failed or didn't happen

--- services/web/postpublish-global.sh
+++ services/web/postpublish-global.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 node ../../tooling/scripts/bin/dockerPublish.js
+PUBLISHED=$?
 
-export $(cat .env.production | xargs)
-
-PACKAGE_VERSION=$(node -pe "'v'+require('./package.json').version")
-
-yarn run sentry-cli releases files ${PACKAGE_VERSION} upload-sourcemaps ./build
+if  [[ ${PUBLISHED} -eq 0 ]]; then
+    export $(cat .env.production | xargs)
+    PACKAGE_VERSION=$(node -pe "'v'+require('./package.json').version")
+    yarn run sentry-cli releases files ${PACKAGE_VERSION} upload-sourcemaps ./build
+fi
