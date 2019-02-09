a586307050568358d2c3e49bc8aba5165a5ac087
doomsower <K.Kuznetcov@gmail.com>
2019-02-09 16:00:13 +0300

    build: fix docker tag commands

--- tooling/postgres-restore-s3/publish.sh
+++ tooling/postgres-restore-s3/publish.sh
@@ -3,6 +3,6 @@
 PACKAGE_VERSION=$(node -pe "'v'+require('./package.json').version")
 
 docker build -t doomsower/postgres-restore-s3:${PACKAGE_VERSION} .
-docker tag -t doomsower/postgres-restore-s3:${PACKAGE_VERSION} doomsower/postgres-restore-s3:latest
+docker tag doomsower/postgres-restore-s3:${PACKAGE_VERSION} doomsower/postgres-restore-s3:latest
 docker push doomsower/postgres-restore-s3:${PACKAGE_VERSION}
 docker push doomsower/postgres-restore-s3:latest
