28f951e2323d3fde8ce8bd966abf2afcd918ddd2
doomsower <K.Kuznetcov@gmail.com>
2019-07-03 10:46:24 +0300

    fix: fix open maps url Android

--- packages/mobile/src/utils/maps/openGoogleMaps.ts
+++ packages/mobile/src/utils/maps/openGoogleMaps.ts
@@ -3,16 +3,23 @@ import { Linking, Platform } from 'react-native';
 import Firebase from 'react-native-firebase';
 
 export const openGoogleMaps = async ([lng, lat]: Coordinate) => {
-  let directionsURL = `geo:0,0?q=${lat},${lng}`;
+  let directionsURL = `https://www.google.com/maps/search/?api=1&query=${lat},${lng}`;
   if (Platform.OS === 'ios') {
     const canOpenGoogleMaps = await Linking.canOpenURL('comgooglemaps://');
-    directionsURL = canOpenGoogleMaps
-      ? `comgooglemaps://?q=${lat},${lng}`
-      : `https://www.google.com/maps/search/?api=1&query=${lat},${lng}`;
+    if (canOpenGoogleMaps) {
+      directionsURL = `comgooglemaps://?q=${lat},${lng}`;
+    }
     Firebase.analytics().setUserProperty(
       'canOpenGoogleMaps',
       canOpenGoogleMaps ? 'true' : 'false',
     );
+  } else {
+    const canOpenGoogleMaps = await Linking.canOpenURL(
+      `geo:0,0?q=${lat},${lng}`,
+    );
+    if (canOpenGoogleMaps) {
+      directionsURL = `geo:0,0?q=${lat},${lng}`;
+    }
   }
   Linking.openURL(directionsURL).catch(() => {
     /*Ignore*/
