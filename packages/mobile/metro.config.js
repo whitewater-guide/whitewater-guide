daeb68a5bfd1f1a794b5edeb1af0430c4f590eee
doomsower <K.Kuznetcov@gmail.com>
2019-07-06 19:46:40 +0300

    build(mobile): disable symlinks in metro config

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -1,26 +1,29 @@
-const path = require('path');
-const metro = require('metro');
+// const path = require('path');
+// const metro = require('metro');
 
 module.exports = {
-  watchFolders: [
-    path.join(process.cwd(), '../clients'),
-    path.join(process.cwd(), '../commons'),
-  ],
-  resolver: {
-    extraNodeModules: new Proxy(
-      {},
-      {
-        get: (target, name) => path.join(process.cwd(), `node_modules/${name}`),
-      },
-    ),
-    blacklistRE: metro.createBlacklist([
-      /@whitewater-guide\/commons\/node_modules\/react-native\/.*/,
-      /@whitewater-guide\/clients\/node_modules\/react-native\/.*/,
-      /@whitewater-guide\/clients\/node_modules\/react\/.*/,
-      new RegExp(path.join(process.cwd(), '../clients/node_modules/') + '.*'),
-      new RegExp(path.join(process.cwd(), '../commons/node_modules/') + '.*'),
-    ]),
-  },
+  // Commented out configuration to support symlinked modules,
+  // since removed symlinking from this project entirely
+  //
+  // watchFolders: [
+  //   path.join(process.cwd(), '../clients'),
+  //   path.join(process.cwd(), '../commons'),
+  // ],
+  // resolver: {
+  //   extraNodeModules: new Proxy(
+  //     {},
+  //     {
+  //       get: (target, name) => path.join(process.cwd(), `node_modules/${name}`),
+  //     },
+  //   ),
+  //   blacklistRE: metro.createBlacklist([
+  //     /@whitewater-guide\/commons\/node_modules\/react-native\/.*/,
+  //     /@whitewater-guide\/clients\/node_modules\/react-native\/.*/,
+  //     /@whitewater-guide\/clients\/node_modules\/react\/.*/,
+  //     new RegExp(path.join(process.cwd(), '../clients/node_modules/') + '.*'),
+  //     new RegExp(path.join(process.cwd(), '../commons/node_modules/') + '.*'),
+  //   ]),
+  // },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
