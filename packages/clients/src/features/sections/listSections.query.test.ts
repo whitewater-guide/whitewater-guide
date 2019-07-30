92d67c257f7cd95c54f2a4363ac184d6bbbdf584
doomsower <K.Kuznetcov@gmail.com>
2019-07-30 19:35:14 +0300

    chore: remove unused stuff

--- packages/clients/src/features/sections/listSections.query.test.ts
+++
@@ -1,8 +0,0 @@
-import { print } from 'graphql';
-import { transformDocument } from 'graphql-ast-tools';
-import { LIST_SECTIONS } from './listSections.query';
-
-it('should generate correct query', () => {
-  const inlinedQuery = transformDocument(LIST_SECTIONS);
-  expect(print(inlinedQuery)).toMatchSnapshot();
-});
