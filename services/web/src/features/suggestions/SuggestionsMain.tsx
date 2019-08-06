076c70cbacf3eefade585773f31fca306dbab9fe
doomsower <K.Kuznetcov@gmail.com>
2019-08-06 21:43:02 +0300

    fix: fix suggestions page refresh

--- services/web/src/features/suggestions/SuggestionsMain.tsx
+++ services/web/src/features/suggestions/SuggestionsMain.tsx
@@ -2,7 +2,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
 import { useAuth } from '@whitewater-guide/clients';
 import { SuggestionStatus } from '@whitewater-guide/commons';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useQuery } from 'react-apollo';
 import { Card } from '../../layout';
 import {
@@ -22,11 +22,14 @@ const ADMIN_STATUSES = [
 const EDITOR_STATUSES = [SuggestionStatus.PENDING];
 
 export const SuggestionsMain: React.FC = () => {
-  const { me } = useAuth();
+  const { me, loading: userLoading } = useAuth();
   const [resolveId, setResolveId] = useState<string | null>(null);
   const [statusFilter, setStatusFilter] = useState(
     me && me.admin ? ADMIN_STATUSES : EDITOR_STATUSES,
   );
+  useEffect(() => {
+    setStatusFilter(me && me.admin ? ADMIN_STATUSES : EDITOR_STATUSES);
+  }, [me && me.admin]);
   const closeResolveDialog = useCallback(() => {
     setResolveId(null);
   }, [setResolveId]);
@@ -42,7 +45,7 @@ export const SuggestionsMain: React.FC = () => {
   );
 
   return (
-    <Card loading={loading && !(data && data.suggestions)}>
+    <Card loading={userLoading || (loading && !(data && data.suggestions))}>
       <CardHeader title="User suggestions" />
       <CardContent>
         <SuggestionsTableInfinite
