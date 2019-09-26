3b6e4754e60ced8b898de1ad6ea63237de60d5b8
doomsower <K.Kuznetcov@gmail.com>
2019-09-26 11:24:27 +0300

    fix: do not store NEW_ID objects in apollo cache

--- packages/clients/src/apollo/dataIdFromObject.ts
+++ packages/clients/src/apollo/dataIdFromObject.ts
@@ -1,6 +1,8 @@
+import { NEW_ID } from '@whitewater-guide/commons';
+
 export function dataIdFromObject(result: any) {
   if (result.__typename) {
-    if (result.id !== undefined) {
+    if (result.id !== undefined && result.id !== NEW_ID) {
       return `${result.__typename}:${result.id}`;
     }
   }
