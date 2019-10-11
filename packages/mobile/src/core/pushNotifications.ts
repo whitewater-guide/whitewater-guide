4e151ea3392377f9421b171c113ee482e24db286
doomsower <K.Kuznetcov@gmail.com>
2019-10-11 16:17:13 +0300

    fix: push notifications

--- packages/mobile/src/core/pushNotifications.ts
+++ packages/mobile/src/core/pushNotifications.ts
@@ -1,34 +1,13 @@
 import messaging from '@react-native-firebase/messaging';
-import { AsyncStorage } from 'react-native';
-
-enum MessagingPermission {
-  UNKNOWN = 1,
-  ENABLED = 2,
-  DISABLED = 3,
-}
 
 export const enablePushNotifications = async () => {
-  let storedPermission = MessagingPermission.UNKNOWN;
-  try {
-    const permsStr = await AsyncStorage.getItem('@ww:pushPermission');
-    if (permsStr) {
-      storedPermission = parseInt(permsStr, 10);
-    }
-  } catch {}
-  if (storedPermission !== MessagingPermission.UNKNOWN) {
-    return;
-  }
-  let enabled = await messaging().hasPermission();
+  const enabled = await messaging().hasPermission();
   if (!enabled) {
     try {
-      await messaging().requestPermission();
-      enabled = true;
+      const result = await messaging().requestPermission();
+      if (result) {
+        await messaging().registerForRemoteNotifications();
+      }
     } catch {}
   }
-  await AsyncStorage.setItem(
-    '@ww:pushPermission',
-    enabled
-      ? MessagingPermission.ENABLED.toString()
-      : MessagingPermission.DISABLED.toString(),
-  );
 };
