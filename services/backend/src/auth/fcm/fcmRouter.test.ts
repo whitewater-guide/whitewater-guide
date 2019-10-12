4b9c3e395f05fb8fca0098c56842311a67eeeb34
doomsower <K.Kuznetcov@gmail.com>
2019-10-12 15:19:46 +0300

    feat: allow replacement of fcm token

--- services/backend/src/auth/fcm/fcmRouter.test.ts
+++ services/backend/src/auth/fcm/fcmRouter.test.ts
@@ -23,12 +23,15 @@ afterEach(async () => {
 describe('set fcm token', () => {
   const ROUTE = '/fcm/set';
 
-  const request = async (fcm_token?: string | null) => {
+  const request = async (
+    fcm_token?: string | null,
+    old_fcm_token?: string | null,
+  ) => {
     const testAgent = agent(app);
     const resp = await testAgent
       .post(ROUTE)
       .set('authorization', `bearer ${getAccessToken(TEST_USER_ID)}`)
-      .send({ fcm_token });
+      .send({ fcm_token, old_fcm_token });
     const tokens = await db(false)
       .select('token')
       .from('fcm_tokens')
@@ -56,5 +59,18 @@ describe('set fcm token', () => {
     const { success, tokens } = await request(null);
     expect(success).toBe(true);
     expect(tokens).toHaveLength(1);
+  });
+
+  it('should replace existing token', async () => {
+    const { success, tokens } = await request('foo', '__user_fcm_token__');
+    expect(tokens).toHaveLength(1);
+    expect(tokens).toContainEqual({ token: 'foo' });
+  });
+
+  it('should not fail on when attempting to replace non-existing token', async () => {
+    const { success, tokens } = await request('foo', 'bar');
+    expect(tokens).toHaveLength(2);
+    expect(tokens).toContainEqual({ token: 'foo' });
+    expect(tokens).toContainEqual({ token: '__user_fcm_token__' });
   });
 });
--- services/backend/src/auth/utils/fcmMiddleware.ts
+++ services/backend/src/auth/utils/fcmMiddleware.ts
@@ -11,8 +11,19 @@ import get from 'lodash/get';
 export const fcmMiddleware: Middleware<any, any> = async (ctx, next) => {
   const token =
     get(ctx, 'request.body.fcm_token') || get(ctx, 'query.fcm_token');
+  const old_token =
+    get(ctx, 'request.body.old_fcm_token') || get(ctx, 'query.old_fcm_token');
   const user_id = get(ctx, 'state.user.id');
   if (token && user_id) {
+    if (old_token) {
+      try {
+        await db()
+          .table('fcm_tokens')
+          .update({ token })
+          .where({ token: old_token });
+        await next();
+      } catch {}
+    }
     const query = db()
       .insert({ user_id, token })
       .into('fcm_tokens')
