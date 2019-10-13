5f40608b958717eed1456898c5baa460301a959f
doomsower <K.Kuznetcov@gmail.com>
2019-10-13 13:06:49 +0300

    feat: quantize coordinates

---
+++ services/backend/src/migrations/030_quantize_coordinates.ts
@@ -0,0 +1,22 @@
+import { createViews, dropViews } from '@db';
+import Knex from 'knex';
+
+const VIEWS = ['gauges', 'sections', 'rivers', 'regions', 'points'];
+/**
+ * Return only 4 digits after point in geometry
+ * https://postgis.net/docs/ST_QuantizeCoordinates.html
+ * @param {Knex} db
+ * @returns {Promise<void>}
+ */
+export const up = async (db: Knex) => {
+  // Change "shape" column
+  await dropViews(db, ...VIEWS);
+  await createViews(db, 30, ...VIEWS);
+};
+
+export const down = async (db: Knex) => {
+  await dropViews(db, ...VIEWS);
+  await createViews(db, 29, ...VIEWS);
+};
+
+export const configuration = { transaction: true };
