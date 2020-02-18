caedbbb43558a73eb83a54ccdc3277ad9633e52f
Konstantin Kuznetsov <K.Kuznetcov@gmail.com>
2020-02-18 18:28:51 +0300

    feat: use new dump/restore

--- tooling/postgres-restore-s3/publish.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-PACKAGE_VERSION=$(node -pe "'v'+require('./package.json').version")
-
-docker build -t doomsower/postgres-restore-s3:${PACKAGE_VERSION} .
-docker tag doomsower/postgres-restore-s3:${PACKAGE_VERSION} doomsower/postgres-restore-s3:latest
-docker push doomsower/postgres-restore-s3:${PACKAGE_VERSION}
-docker push doomsower/postgres-restore-s3:latest
