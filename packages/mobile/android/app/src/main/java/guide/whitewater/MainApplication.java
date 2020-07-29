e9399191097d9a3f5f3809a94a728bcff08abd33
Konstantin Kuznetsov <K.Kuznetcov@gmail.com>
2020-07-29 20:55:00 +0300

    feat: remove code-push

--- packages/mobile/android/app/src/main/java/guide/whitewater/MainApplication.java
+++ packages/mobile/android/app/src/main/java/guide/whitewater/MainApplication.java
@@ -8,7 +8,6 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.microsoft.codepush.react.CodePush;
 import com.reactnativeultimateconfig.UltimateConfigModule;
 import com.rnfs.RNFSPackage;
 
@@ -38,13 +37,6 @@ public class MainApplication extends Application implements ReactApplication {
                 return "index";
             }
 
-            // 2. Override the getJSBundleFile method in order to let
-            // the CodePush runtime determine where to get the JS
-            // bundle location from on each app start
-            @Override
-            protected String getJSBundleFile() {
-                return CodePush.getJSBundleFile();
-            }
         };
 
     @Override
--- packages/mobile/src/utils/versioning.ts
+++ packages/mobile/src/utils/versioning.ts
@@ -1,45 +1,11 @@
 import AsyncStorage from '@react-native-community/async-storage';
-import CodePush from 'react-native-code-push';
-import Config from 'react-native-ultimate-config';
-import { Platform } from 'react-native';
 import { useEffect } from 'react';
-
-export interface CodePushVersion {
-  local: string | null;
-  pending: string | null;
-  remote: string | null;
-}
+import { Platform } from 'react-native';
+import Config from 'react-native-ultimate-config';
 
 class Versioning {
-  private _codePushVersion!: CodePushVersion;
-
-  getCodePushVersion = async () => {
-    if (this._codePushVersion) {
-      return Promise.resolve(this._codePushVersion);
-    }
-    try {
-      const localPackage = await CodePush.getUpdateMetadata(
-        CodePush.UpdateState.RUNNING,
-      );
-      const pendingPackage = await CodePush.getUpdateMetadata(
-        CodePush.UpdateState.PENDING,
-      );
-      const remotePackage = await CodePush.checkForUpdate();
-      this._codePushVersion = {
-        local: localPackage ? localPackage.label : null,
-        pending: pendingPackage ? pendingPackage.label : null,
-        remote: remotePackage ? remotePackage.label : null,
-      };
-    } catch (e) {
-      this._codePushVersion = { local: null, pending: null, remote: null };
-    }
-    return this._codePushVersion;
-  };
-
   getHumanVersion = async () => {
-    // prefer ios build number, because they're incremented together but ios one looks better
-    const { local, pending, remote } = await this.getCodePushVersion();
-    return `${PJSON_VERSION} ${local || pending || remote || 'v0'}`;
+    return PJSON_VERSION;
   };
 
   getDist = () => {
@@ -51,11 +17,7 @@ class Versioning {
   };
 
   getSentryVersion = async () => {
-    const { local, pending, remote } = await this.getCodePushVersion();
-    return `${PJSON_VERSION}-${Config.ENV_NAME}-${local ||
-      pending ||
-      remote ||
-      'v0'}`;
+    return `${PJSON_VERSION}-${Config.ENV_NAME}`;
   };
 }
 
