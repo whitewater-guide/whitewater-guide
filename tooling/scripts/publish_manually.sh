74c9c80825c00e7af5623efc4bb20b116e6f5e44
Konstantin Kuznetsov <K.Kuznetcov@gmail.com>
2020-09-20 12:07:48 +0300

    build: publish to ghcr.io

--- tooling/scripts/publish_manually.sh
+++ tooling/scripts/publish_manually.sh
@@ -4,5 +4,5 @@ echo "Manually publishing backend to github packages"
 
 PACKAGE_VERSION=$(node -p -e "require('./packages/backend/package.json').version")
 
-docker build -t docker.pkg.github.com/whitewater-guide/whitewater-guide/backend:${PACKAGE_VERSION} -f ./packages/backend/Dockerfile .
-docker push docker.pkg.github.com/whitewater-guide/whitewater-guide/backend:${PACKAGE_VERSION}
+docker build -t ghcr.io/whitewater-guide/backend:${PACKAGE_VERSION} -f ./packages/backend/Dockerfile .
+docker push ghcr.io/whitewater-guide/backend:${PACKAGE_VERSION}
