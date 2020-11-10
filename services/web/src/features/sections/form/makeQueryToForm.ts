43e1dc709dba264a55358cb2d25bc9e5655e5ffa
Konstantin Kuznetsov <K.Kuznetcov@gmail.com>
2020-11-10 18:04:15 +0300

    feat: improve section duplication experience
    closes #519

--- services/web/src/features/sections/form/makeQueryToForm.ts
+++ services/web/src/features/sections/form/makeQueryToForm.ts
@@ -1,4 +1,4 @@
-import { Tag } from '@whitewater-guide/commons';
+import { CoordinateLoose, NamedNode, Tag } from '@whitewater-guide/commons';
 import { fromMarkdown } from '@whitewater-guide/md-editor';
 import { flow, groupBy } from 'lodash/fp';
 import { toNamedNode } from '../../../formik/utils';
@@ -15,53 +15,70 @@ const groupTags = (tags: Tag[]) => {
   };
 };
 
+const simplifyShape = (shape: CoordinateLoose[]): CoordinateLoose[] => {
+  return [shape[0], shape[shape.length - 1]];
+};
+
+const emptySection = (region: NamedNode, river: any): SectionFormData => {
+  return {
+    id: null,
+    river,
+    region: toNamedNode(region),
+    name: '',
+    altNames: [],
+    season: null,
+    seasonNumeric: [],
+    gauge: null,
+    flowsText: null,
+    shape: [],
+    distance: null,
+    drop: null,
+    duration: null,
+    difficulty: 0,
+    difficultyXtra: null,
+    rating: null,
+    pois: [],
+    description: fromMarkdown(null),
+    hidden: false,
+    helpNeeded: null,
+    hazardsTags: [],
+    kayakingTags: [],
+    miscTags: [],
+    supplyTags: [],
+    levels: null,
+    flows: null,
+    media: [],
+  };
+};
+
 export default (isCopy?: boolean) => (result: QResult): SectionFormData => {
   if (!result || !result.section) {
     // Deliberately allow null. Initial form value will be invalid
     const river: any = (result && result.river) || { id: null, name: '' };
-    return {
-      id: null,
-      river,
-      region: toNamedNode(result.region),
-      name: '',
-      altNames: [],
-      season: null,
-      seasonNumeric: [],
-      gauge: null,
-      flowsText: null,
-      shape: [],
-      distance: null,
-      drop: null,
-      duration: null,
-      difficulty: 0,
-      difficultyXtra: null,
-      rating: null,
-      pois: [],
-      description: fromMarkdown(null),
-      hidden: false,
-      helpNeeded: null,
-      hazardsTags: [],
-      kayakingTags: [],
-      miscTags: [],
-      supplyTags: [],
-      levels: null,
-      flows: null,
-      media: [],
-    };
+    return emptySection(result.region, river);
   }
   const {
     tags,
-    id,
-    name,
     description,
     demo,
     media,
     verified,
     ...section
   } = result.section;
+
+  if (isCopy) {
+    return {
+      ...emptySection(result.region, toNamedNode(section.river)),
+      shape: simplifyShape(section.shape),
+      description: fromMarkdown(description),
+      season: section.season,
+      seasonNumeric: section.seasonNumeric,
+      gauge: section.gauge,
+      ...groupTags(tags),
+    };
+  }
+
   return {
-    id: isCopy ? null : id,
-    name: isCopy ? '' : name,
     ...section,
     media: media.nodes,
     river: toNamedNode(section.river),
