b975903d77db3fc1c720f3024961075e59be96a9
Konstantin Kuznetsov <K.Kuznetcov@gmail.com>
2020-11-28 18:11:36 +0300

    fix: content bucket in production

--- packages/backend/src/s3/paths.ts
+++ packages/backend/src/s3/paths.ts
@@ -1,3 +1,4 @@
+import config from '~/config';
 /* eslint-disable node/no-process-env */
 export type S3Prefix = 'temp' | 'media' | 'avatars' | 'covers' | 'banners';
 
@@ -5,7 +6,7 @@ const isTest = process.env.JEST_WORKER_ID;
 
 export const CONTENT_BUCKET = isTest
   ? `content-${process.env.JEST_WORKER_ID}`
-  : 'content';
+  : config.contentPublicURL;
 
 export const TEMP = 'temp';
 export const MEDIA = 'media';
