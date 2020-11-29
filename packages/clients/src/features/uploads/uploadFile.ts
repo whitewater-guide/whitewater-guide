43990f978999d3a525971416acaa5103621c5b58
Konstantin Kuznetsov <K.Kuznetcov@gmail.com>
2020-11-29 16:28:50 +0300

    fix: upload file to use s3

--- packages/clients/src/features/uploads/uploadFile.ts
+++ packages/clients/src/features/uploads/uploadFile.ts
@@ -62,7 +62,8 @@ export const uploadFile = async (
       signal: abortController ? abortController.signal : undefined,
     });
     if (resp.status === 204) {
-      return resp.headers.get('Location')!;
+      const location = resp.headers.get('Location')!;
+      return decodeURIComponent(location).split('/').pop()!;
     }
     const text = await resp.text();
     throw new UploadFileError(text, resp.status);
