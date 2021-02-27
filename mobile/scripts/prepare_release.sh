2b2f19700fc2dab6787dcd4cdb707c683aaffad0
Konstantin Kuznetsov <K.Kuznetcov@gmail.com>
2021-02-27 15:42:04 +0300

    fix: fix release script

--- mobile/scripts/prepare_release.sh
+++ mobile/scripts/prepare_release.sh
@@ -10,5 +10,4 @@ git add package.json
 VERSION=$(node -p "require('./package.json').version")
 fastlane ios bump skipCommit:true --env staging
 fastlane android bump skipCommit:true --env staging
-node ../../tooling/scripts/bin/changelog.js --path .
 git commit -m "chore(mobile): prepare release ${VERSION}"
