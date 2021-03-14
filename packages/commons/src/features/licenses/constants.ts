3f6bf0dded5901ead20bb19370a62054d2969103
Konstantin Kuznetsov <K.Kuznetcov@gmail.com>
2021-03-14 20:12:53 +0300

    fix: shorter names for common licenses

--- packages/commons/src/features/licenses/constants.ts
+++ packages/commons/src/features/licenses/constants.ts
@@ -1,45 +1,41 @@
 import { License } from './types';
 
-export const COMMON_LICENSES: License[] = [
+export type CommonLicense = Omit<License, 'slug'> & { slug: string };
+
+export const COMMON_LICENSES: CommonLicense[] = [
   {
     slug: 'CC_BY',
-    name:
-      'Creative Commons Attribution 4.0 International Public License (CC BY 4.0)',
+    name: 'CC BY 4.0',
     url: 'https://creativecommons.org/licenses/by/4.0/',
   },
   {
     slug: 'CC_BY-SA',
-    name:
-      'Creative Commons Attribution-ShareAlike 4.0 International Public License (CC BY-SA 4.0)',
+    name: 'CC BY-SA 4.0',
     url: 'https://creativecommons.org/licenses/by-sa/4.0/',
   },
   {
     slug: 'CC_BY-ND',
-    name:
-      'Creative Commons Attribution-NoDerivatives 4.0 International Public License (CC BY-ND 4.0)',
+    name: 'CC BY-ND 4.0',
     url: 'https://creativecommons.org/licenses/by-nd/4.0/',
   },
   {
     slug: 'CC_BY-NC',
-    name:
-      'Creative Commons Attribution-NonCommercial 4.0 International Public License (CC BY-NC 4.0)',
+    name: 'CC BY-NC 4.0',
     url: 'https://creativecommons.org/licenses/by-nc/4.0/',
   },
   {
     slug: 'CC_BY-NC-SA',
-    name:
-      'Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public License (CC BY-NC-SA 4.0)',
+    name: 'CC BY-NC-SA 4.0',
     url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
   },
   {
     slug: 'CC_BY-NC-ND',
-    name:
-      'Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International Public License (CC BY-NC-ND 4.0)',
+    name: 'CC BY-NC-ND 4.0',
     url: 'https://creativecommons.org/licenses/by-nc-nd/4.0/',
   },
   {
     slug: 'CC0',
-    name: 'CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+    name: 'CC0 1.0',
     url: 'https://creativecommons.org/publicdomain/zero/1.0/',
   },
 ];
@@ -47,12 +43,9 @@ export const COMMON_LICENSES: License[] = [
 // This is the default license for any content in the app
 export const ROOT_LICENSE: License = {
   slug: 'CC_BY-NC-SA',
-  name:
-    'Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public License (CC BY-NC-SA 4.0)',
+  name: 'CC BY-NC-SA 4.0',
   url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
 };
-
-export type CommonLicense = Omit<License, 'slug'> & { slug: string };
 
 /**
  * Returns true if license is common and it's possible to display logo
