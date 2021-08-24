ae6bef6e1cd30c07dbcb6d0f5cd443447fce533d
doomsower <K.Kuznetcov@gmail.com>
2021-08-24 19:18:00 +0300

    refactor: use new react-native-modal-popover version

--- packages/mobile/src/screens/section/chart/GaugeValueWarning.tsx
+++ packages/mobile/src/screens/section/chart/GaugeValueWarning.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { Popover, PopoverController } from 'react-native-modal-popover';
+import { Popover, usePopover } from 'react-native-modal-popover';
 
 import Icon from '~/components/Icon';
-
-import theme from '../../../theme';
+import theme from '~/theme';
 
 const styles = StyleSheet.create({
   popoverContent: {
@@ -13,34 +12,28 @@ const styles = StyleSheet.create({
   },
 });
 
-const GaugeWarning: React.FC = ({ children }) => (
-  <PopoverController>
-    {({
-      openPopover,
-      closePopover,
-      popoverVisible,
-      setPopoverAnchor,
-      popoverAnchorRect,
-    }) => (
-      <>
-        <Icon
-          icon="alert"
-          size={16}
-          ref={setPopoverAnchor}
-          onPress={openPopover}
-        />
-        <Popover
-          useNativeDriver
-          contentStyle={styles.popoverContent}
-          visible={popoverVisible}
-          onClose={closePopover}
-          fromRect={popoverAnchorRect}
-        >
-          {children}
-        </Popover>
-      </>
-    )}
-  </PopoverController>
-);
+const GaugeWarning: React.FC = ({ children }) => {
+  const {
+    openPopover,
+    closePopover,
+    popoverVisible,
+    touchableRef,
+    popoverAnchorRect,
+  } = usePopover();
+  return (
+    <>
+      <Icon icon="alert" size={16} ref={touchableRef} onPress={openPopover} />
+      <Popover
+        useNativeDriver
+        contentStyle={styles.popoverContent}
+        visible={popoverVisible}
+        onClose={closePopover}
+        fromRect={popoverAnchorRect}
+      >
+        {children}
+      </Popover>
+    </>
+  );
+};
 
 export default GaugeWarning;
