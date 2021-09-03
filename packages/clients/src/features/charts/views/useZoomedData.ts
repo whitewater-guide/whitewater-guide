43c559418df07c6172419e83ef91d21ddb2bb6c2
Konstantin Kuznetsov <K.Kuznetcov@gmail.com>
2021-09-03 10:21:46 +0300

    fix: always display latest point in chart

--- packages/clients/src/features/charts/views/useZoomedData.ts
+++ packages/clients/src/features/charts/views/useZoomedData.ts
@@ -15,7 +15,7 @@ function useZoomedData(
 
     const startIndex = data.findIndex((d) => d.timestamp >= zoomedXDomain[0]);
     const endIndex = data.findIndex((d) => d.timestamp > zoomedXDomain[1]);
-    const filtered = data.slice(
+    let filtered = data.slice(
       Math.max(startIndex - 1, 0),
       endIndex === -1 ? -1 : endIndex + 1,
     );
@@ -25,10 +25,19 @@ function useZoomedData(
       // limit k to powers of 2, e.g. 64, 128, 256
       // so that the same points will be chosen reliably, reducing flicker
       const k = Math.pow(2, Math.ceil(Math.log2(filtered.length / maxPoints)));
-      return filtered.filter(
+      filtered = filtered.filter(
         // ensure modulo is always calculated from same reference: i + startIndex
         (_, i) => (i + startIndex) % k === 0,
       );
+
+      // Ensure that latest point always is preset
+      const latest = endIndex === -1 ? data[data.length - 1] : data[endIndex];
+      if (
+        filtered[filtered.length - 1].timestamp.getTime() !==
+        latest.timestamp.getTime()
+      ) {
+        filtered.push(latest);
+      }
     }
     return filtered;
   }, [data, zoomedDomain, width, maxDensity]);
