86f2394722c7ae1f37d326a18181e025f1f2c471
Konstantin Kuznetsov <K.Kuznetcov@gmail.com>
2021-09-04 12:32:53 +0300

    build: update dump script

--- services/db/scripts/refresh_dump.sh
+++ services/db/scripts/refresh_dump.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
-set -e
 
-set -o allexport
-source .env
-set +o allexport
+# This script finds latest backup, downloads it, extracts it into dump folder
+# Then dev database can use data from production
+
+set -e
 
 rm -rf config/dump/*
 
-S3_PREFIX=$1
+S3_BUCKET=$1
+AWS_PROFILE=$2
 DUMP_NAME="backup.tar"
 
-PGDUMP_SERVICE=$(docker-machine ssh ww-${S3_PREFIX} docker service ps wwguide_pgdump --filter="desired-state=running" -q)
-PGDUMP_CONTAINER=$(docker-machine ssh ww-${S3_PREFIX} docker ps --filter="name=${PGDUMP_SERVICE}" -q)
-CMD="docker-machine ssh ww-${S3_PREFIX} docker exec ${PG_EXTRA} ${PGDUMP_CONTAINER} sh /app/backup_partial.sh"
-eval "${CMD}"
-
-LATEST_BACKUP=$(aws2 s3 ls s3://$S3_BUCKET/$S3_PREFIX/ | grep partial | sort | tail -n 1 | awk '{ print $4 }')
+LATEST_BACKUP=$(aws s3 ls s3://$S3_BUCKET --profile $AWS_PROFILE | sort | tail -n 1 | awk '{ print $4 }')
 
 echo "Fetching ${LATEST_BACKUP} from S3"
-aws2 s3 cp s3://$S3_BUCKET/$S3_PREFIX/${LATEST_BACKUP} config/dump/${DUMP_NAME}
+aws s3 cp s3://$S3_BUCKET/${LATEST_BACKUP} --profile $AWS_PROFILE config/dump/${DUMP_NAME}
 cd config/dump
 tar -xvf ${DUMP_NAME}
 rm ${DUMP_NAME}
+# dev database doesn't use it, because it's too big for tmpfs database
+rm config/dump/gorge.bak
 echo "Success"
