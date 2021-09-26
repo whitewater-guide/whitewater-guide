f493851908f9daa7e79bf94c4913b1b12b4db0a9
doomsower <K.Kuznetcov@gmail.com>
2021-09-26 14:54:39 +0300

    build: remove git add from lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'git add'],
-  '**/*.{json,md,yaml,yml}': ['prettier --write', 'git add'],
+  '**/*.{js,jsx,ts,tsx}': ['eslint --fix'],
+  '**/*.{json,md,yaml,yml}': ['prettier --write'],
 };
