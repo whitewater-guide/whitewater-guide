1101e4a0711f1fa484fb2dd39cc443462c4c1e83
doomsower <K.Kuznetcov@gmail.com>
2021-11-21 16:52:49 +0300

    chore: update deps and splashscreen

--- packages/mobile/android/app/src/main/java/guide/whitewater/MainActivity.java
+++ packages/mobile/android/app/src/main/java/guide/whitewater/MainActivity.java
@@ -1,6 +1,5 @@
 package guide.whitewater;
 
-import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -19,18 +18,17 @@ public class MainActivity extends ReactActivity {
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(null);
-        RNBootSplash.init(R.drawable.bootsplash, MainActivity.this);
-    }
-
-
-    @Override
     protected ReactActivityDelegate createReactActivityDelegate() {
         return new ReactActivityDelegate(this, getMainComponentName()) {
             @Override
             protected ReactRootView createRootView() {
                 return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+
+            @Override
+            protected void loadApp(String appKey) {
+                RNBootSplash.init(MainActivity.this);
+                super.loadApp(appKey);
             }
         };
     }
