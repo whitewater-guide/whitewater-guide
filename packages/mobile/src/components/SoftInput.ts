6edcc89d4628e4fd898bef622e4bb21ed776b5cd
Konstantin Kuznetsov <K.Kuznetcov@gmail.com>
2022-02-07 18:07:46 +0300

    fix: soft input on ios

--- packages/mobile/src/components/SoftInput.ts
+++ packages/mobile/src/components/SoftInput.ts
@@ -1,19 +1,27 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 
 const { SoftInputModule } = NativeModules;
-
-const { ADJUST_PAN, ADJUST_RESIZE } = SoftInputModule.getConstants();
 
 export enum SoftInputMode {
   ADJUST_PAN,
   ADJUST_RESIZE,
 }
 
-const modes = {
-  [SoftInputMode.ADJUST_PAN]: ADJUST_PAN,
-  [SoftInputMode.ADJUST_RESIZE]: ADJUST_RESIZE,
-};
+let _modes: Record<SoftInputMode, unknown>;
+
+function getModeValue(mode: SoftInputMode) {
+  if (!_modes) {
+    const { ADJUST_PAN, ADJUST_RESIZE } = SoftInputModule.getConstants();
+    _modes = {
+      [SoftInputMode.ADJUST_PAN]: ADJUST_PAN,
+      [SoftInputMode.ADJUST_RESIZE]: ADJUST_RESIZE,
+    };
+  }
+  return _modes[mode];
+}
 
 export function setSoftInputMode(mode: SoftInputMode): void {
-  NativeModules.SoftInputModule.setSoftInputMode(modes[mode]);
+  if (Platform.OS === 'android') {
+    NativeModules.SoftInputModule.setSoftInputMode(getModeValue(mode));
+  }
 }
