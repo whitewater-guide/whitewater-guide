1a8233098df3e92d641b6798a41676adcd566124
doomsower <K.Kyznetcov@gmail.com>
2022-12-10 17:43:52 +0300

    fix: localhost ios

--- packages/mobile/src/utils/urls.ts
+++ packages/mobile/src/utils/urls.ts
@@ -3,7 +3,9 @@ import Config from 'react-native-ultimate-config';
 
 // workaround for local debugging on connected ios device
 function lh(host: string): string {
-  return host === 'localhost' && Platform.OS === 'ios' ? '192.168.1.100' : host;
+  return __DEV__ && Platform.OS === 'ios'
+    ? host.replace('localhost:', '192.168.1.100:')
+    : host;
 }
 
 export const BACKEND_URL = `${Config.BACKEND_PROTOCOL}://${lh(
@@ -12,4 +14,5 @@ export const BACKEND_URL = `${Config.BACKEND_PROTOCOL}://${lh(
 export const CHAT_URL = `${Config.BACKEND_PROTOCOL}://${lh(Config.CHAT_HOST)}`;
 
 export const WEB_URL = BACKEND_URL.replace('api.', '');
+export const STATIC_CONTENT_URL_BASE = lh(Config.STATIC_CONTENT_URL_BASE);
 export const DEEP_LINKING_URL = `${Config.BACKEND_PROTOCOL}://${Config.DEEP_LINKING_DOMAIN}`;
