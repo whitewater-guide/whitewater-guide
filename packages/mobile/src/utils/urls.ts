92bb8b720b4de203a0ede70f4af9c8f21ede28b4
doomsower <K.Kyznetcov@gmail.com>
2022-12-10 17:24:55 +0300

    fix: workaround for local debugging on iphone

--- packages/mobile/src/utils/urls.ts
+++ packages/mobile/src/utils/urls.ts
@@ -1,7 +1,15 @@
+import { Platform } from 'react-native';
 import Config from 'react-native-ultimate-config';
 
-export const BACKEND_URL = `${Config.BACKEND_PROTOCOL}://${Config.BACKEND_HOST}`;
-export const CHAT_URL = `${Config.BACKEND_PROTOCOL}://${Config.CHAT_HOST}`;
+// workaround for local debugging on connected ios device
+function lh(host: string): string {
+  return host === 'localhost' && Platform.OS === 'ios' ? '192.168.1.100' : host;
+}
+
+export const BACKEND_URL = `${Config.BACKEND_PROTOCOL}://${lh(
+  Config.BACKEND_HOST,
+)}`;
+export const CHAT_URL = `${Config.BACKEND_PROTOCOL}://${lh(Config.CHAT_HOST)}`;
 
 export const WEB_URL = BACKEND_URL.replace('api.', '');
 export const DEEP_LINKING_URL = `${Config.BACKEND_PROTOCOL}://${Config.DEEP_LINKING_DOMAIN}`;
