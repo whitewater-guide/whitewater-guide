208d92932598986b8f8106600eb412490ede7f7a
doomsower <K.Kuznetcov@gmail.com>
2023-11-05 19:37:20 -0300

    fix: section header title

--- packages/mobile/src/screens/section/SectionTitle.tsx
+++ packages/mobile/src/screens/section/SectionTitle.tsx
@@ -1,9 +1,17 @@
 import { sectionName } from '@whitewater-guide/clients';
 import type { SectionNameShortFragment } from '@whitewater-guide/schema';
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+
+import theme from '~/theme';
 
 import getTitleFontSize from '../../utils/getTitleFontSize';
+
+const styles = StyleSheet.create({
+  title: {
+    color: theme.colors.textLight,
+  },
+});
 
 interface Props {
   section?: Partial<SectionNameShortFragment> | null;
@@ -12,7 +20,9 @@ interface Props {
 const SectionTitle: React.FC<Props> = ({ section }) => {
   const fullName = sectionName(section);
   return (
-    <Text style={{ fontSize: getTitleFontSize(fullName) }}>{fullName}</Text>
+    <Text style={[styles.title, { fontSize: getTitleFontSize(fullName) }]}>
+      {fullName}
+    </Text>
   );
 };
 
